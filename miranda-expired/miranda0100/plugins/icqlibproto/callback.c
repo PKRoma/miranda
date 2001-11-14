7fd13ef8d391cae9954d8b132d621f663dc552ff
Richard Hughes <cyreve@users.sourceforge.net>
2001-11-14 23:45:29 +0000

    remove old Translate() definition for plugins to 0.1.0.1
    git-svn-id: http://miranda.googlecode.com/svn/trunk@129 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda-expired/miranda0100/plugins/icqlibproto/callback.c
+++ miranda-expired/miranda0100/plugins/icqlibproto/callback.c
@@ -32,7 +32,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../../miranda32/random/ignore/m_ignore.h"
 #include "../../miranda32/random/langpack/m_langpack.h"
 #include "icqproto.h"
-#define Translate(s)   ((char*)CallService(MS_LANGPACK_TRANSLATESTRING,0,(LPARAM)(s)))
 
 extern int icqNetTestTimerId,icqConnectTimeoutId,icqConnectRetries;
 extern int icqConnectStatusMode;
