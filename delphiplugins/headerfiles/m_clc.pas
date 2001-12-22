5d9b0c98ddfe1b4e283c33da941adac070be4b34
Christian Kästner <christian.k@stner.de>
2001-12-22 17:46:49 +0000

    empty headerfiles added (not converted yet)
    git-svn-id: http://miranda.googlecode.com/svn/trunk/miranda-expired@132 83df23c2-51fd-11de-bb24-2d5e9b4af41d

---
+++ delphiplugins/headerfiles/m_clc.pas
@@ -0,0 +1,12 @@
+{***************************************************************
+ * Project     : Miranda Plugin API for Delphi
+ * Description : Converted Headerfile
+ *
+ * Author      : Christian Kästner
+ * Date        : 22.12.2001
+ *
+ * Copyright © 2001 by Christian Kästner (christian.k@stner.de)
+ ****************************************************************}
+
+
+//not translated yet
