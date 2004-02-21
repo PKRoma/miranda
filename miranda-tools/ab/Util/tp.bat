182674f2a23e8e7b54f00d8412e76a65dcd21429
Sam Kothari <egodust@miranda-im.org>
2004-02-21 01:52:42 +0000

    ab: binaries from TortoiseCVS/PuTTY, put here for ease of reference
    git-svn-id: http://miranda.googlecode.com/svn/trunk@183 83df23c2-51fd-11de-bb24-2d5e9b4af41d

---
+++ miranda-tools/ab/Util/tp.bat
@@ -0,0 +1,6 @@
+@echo off
+REM //
+REM // This script redirects output to plink with the password as a command line option
+REM // since it can not be done via python because the env() option can not take any flags
+REM //
+c:\python\python.exe tp.py PASSWORD %1 %2 %3 %4 %5 %6 %7 %8 %9
