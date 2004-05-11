c2de28aa28a96d52869d88f25c1794c34587ce2f
Sam Kothari <egodust@miranda-im.org>
2004-05-11 19:23:33 +0000

    Don't need to include PASSWORD as an argument to script
    git-svn-id: http://miranda.googlecode.com/svn/trunk@208 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda-tools/ab/Util/tp.bat
+++ miranda-tools/ab/Util/tp.bat
@@ -3,4 +3,4 @@ REM //
 REM // This script redirects output to plink with the password as a command line option
 REM // since it can not be done via python because the env() option can not take any flags
 REM //
-c:\python\python.exe tp.py PASSWORD %1 %2 %3 %4 %5 %6 %7 %8 %9
+c:\python\python.exe tp.py %1 %2 %3 %4 %5 %6 %7 %8 %9
