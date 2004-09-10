363b7f2fff1a235613db6912fa3b236ca000ef31
Sam Kothari <egodust@miranda-im.org>
2004-09-10 18:33:29 +0000

    Removed script used tools
    git-svn-id: http://miranda.googlecode.com/svn/trunk@211 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda-tools/ab/Util/tp.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM //
-REM // This script redirects output to plink with the password as a command line option
-REM // since it can not be done via python because the env() option can not take any flags
-REM //
-c:\python\python.exe tp.py %1 %2 %3 %4 %5 %6 %7 %8 %9
