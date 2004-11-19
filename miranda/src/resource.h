cf98502158e179a9bcdb3aa0ff93efdc75055930
Sam Kothari <egodust@miranda-im.org>
2004-11-19 12:29:31 +0000

    Made idle options saner, changed AA stuff
    git-svn-id: http://miranda.googlecode.com/svn/trunk@382 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/resource.h
+++ miranda/src/resource.h
@@ -489,6 +489,7 @@
 #define IDC_PLUGINCPYR                  1681
 #define IDC_PLUGINURL                   1682
 #define IDC_PLUGINEMAIL                 1684
+#define IDC_IDLESPIN                    1687
 #define IDI_SEARCHALL                   32548
 #define ID_ICQ_EXIT                     40001
 #define IDM_COPY                        40001
@@ -519,7 +520,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        274
 #define _APS_NEXT_COMMAND_VALUE         40018
-#define _APS_NEXT_CONTROL_VALUE         1686
+#define _APS_NEXT_CONTROL_VALUE         1690
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
