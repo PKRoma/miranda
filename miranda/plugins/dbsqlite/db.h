f71f43c3004949d51c41bac32ebb43d691c46768
Sam Kothari <egodust@miranda-im.org>
2004-12-08 17:40:16 +0000

    dbsqlite: this isn't finished but you can start a new profile and have global settings, but no contacts, history or settings per contact.
    You also need to download sqlite3 (see README)
    
    
    git-svn-id: http://miranda.googlecode.com/svn/trunk@488 83df23c2-51fd-11de-bb24-2d5e9b4af41d

---
+++ miranda/plugins/dbsqlite/db.h
@@ -0,0 +1,12 @@
+/**/
+
+// db.c
+int dbsqlite_LoadDatabase(char * profile);
+int dbsqlite_UnloadDatabase(void);
+
+
+// dbcache.c
+void InitCache(void);
+void UninitCache(void);
+int DbCache_GetSetting(HANDLE hContact, const char * szModule, const char * szSetting, int staticAlloc, DBVARIANT * pValue);
+int DbCache_WriteSetting(HANDLE hContact, const char * szModule, const char * szSetting, DBVARIANT * dbv);
