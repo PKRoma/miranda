ed24e009f55059981161f035c26b9c607b1bb6ee
Josef Kučera <jokusoftware@users.sourceforge.net>
2005-01-15 20:33:20 +0000

    SAFE_FREE finally safe, new NETLIBOPENCONNECTION fix
    git-svn-id: http://miranda.googlecode.com/svn/trunk@590 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/IcqOscarJ/icq_server.c
+++ miranda/protocols/IcqOscarJ/icq_server.c
@@ -1,21 +1,22 @@
 // ---------------------------------------------------------------------------80
 //                ICQ plugin for Miranda Instant Messenger
 //                ________________________________________
-// 
+//
 // Copyright © 2000,2001 Richard Hughes, Roland Rabien, Tristan Van de Vreede
 // Copyright © 2001,2002 Jon Keating, Richard Hughes
 // Copyright © 2002,2003,2004 Martin Öberg, Sam Kothari, Robert Rainwater
-// 
+// Copyright © 2004,2005 Joe Kucera
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -29,7 +30,7 @@
 //
 // DESCRIPTION:
 //
-//  Describe me here please...
+//  Manages main server connection, low-level communication
 //
 // -----------------------------------------------------------------------------
 
@@ -66,7 +67,7 @@ static DWORD __stdcall icq_serverThread(serverthread_start_info* infoParam)
 
 
 	info = *infoParam;
-	SAFE_FREE(infoParam);
+	SAFE_FREE(&infoParam);
 
 	srand(time(NULL));
 
@@ -78,7 +79,7 @@ static DWORD __stdcall icq_serverThread(serverthread_start_info* infoParam)
 	// Connect to the login server
 	Netlib_Logf(ghServerNetlibUser, "Authenticating to server");
 	hServerConn = (HANDLE)CallService(MS_NETLIB_OPENCONNECTION, (WPARAM)ghServerNetlibUser, (LPARAM)&info.nloc);
-	SAFE_FREE((void*)info.nloc.szHost);
+	SAFE_FREE(&(void*)info.nloc.szHost);
 
 
 	// Login error
@@ -97,8 +98,8 @@ static DWORD __stdcall icq_serverThread(serverthread_start_info* infoParam)
 
 		return 0;
 	}
-	
-	
+
+
 	// Initialize direct connection ports
 	{
 		NETLIBBIND nlb = {0};
@@ -152,7 +153,7 @@ static DWORD __stdcall icq_serverThread(serverthread_start_info* infoParam)
 
 		// Close DC port
 		Netlib_CloseHandle(hDirectBoundPort);
-		
+
 	}
 
 
@@ -162,20 +163,19 @@ static DWORD __stdcall icq_serverThread(serverthread_start_info* infoParam)
 	if (gnCurrentStatus != ID_STATUS_OFFLINE)
 	{
 		int oldStatus = gnCurrentStatus;
-		
+
 		gnCurrentStatus = ID_STATUS_OFFLINE;
 		ProtoBroadcastAck(gpszICQProtoName, NULL, ACKTYPE_STATUS, ACKRESULT_SUCCESS,
 			(HANDLE)oldStatus, gnCurrentStatus);
 	}
 
-
 	// Offline all contacts
 	{
 		HANDLE hContact;
 		char* szProto;
-		
+
 		hContact= (HANDLE)CallService(MS_DB_CONTACT_FINDFIRST, 0, 0);
-		
+
 		while (hContact)
 		{
 			szProto = (char*)CallService(MS_PROTO_GETCONTACTBASEPROTO, (WPARAM)hContact, 0);
@@ -186,12 +186,12 @@ static DWORD __stdcall icq_serverThread(serverthread_start_info* infoParam)
 					if (DBGetContactSettingWord(hContact, gpszICQProtoName, "Status", ID_STATUS_OFFLINE)
 						!= ID_STATUS_OFFLINE)
 					{
-						DBWriteContactSettingWord(hContact, gpszICQProtoName, "Status", 
+						DBWriteContactSettingWord(hContact, gpszICQProtoName, "Status",
 							ID_STATUS_OFFLINE);
 					}
 				}
 			}
-			
+
 			hContact = (HANDLE)CallService(MS_DB_CONTACT_FINDNEXT, (WPARAM)hContact, 0);
 		}
 	}
@@ -206,7 +206,7 @@ void icq_serverDisconnect(void)
 {
 
 	EnterCriticalSection(&connectionHandleMutex);
-	
+
 	if (hServerConn)
 	{
 		Netlib_CloseHandle(hServerConn);
@@ -248,8 +248,8 @@ static int handleServerPackets(unsigned char* buf, int len, serverthread_start_i
 
 		if (len < 6 + datalen)
 			break;
-		
-		
+
+
 #ifdef _DEBUG
 		Netlib_Logf(ghServerNetlibUser, "Server FLAP: Channel %u, Seq %u, Length %u bytes", channel, sequence, datalen);
 #endif
@@ -259,34 +259,34 @@ static int handleServerPackets(unsigned char* buf, int len, serverthread_start_i
 		case ICQ_LOGIN_CHAN:
 			handleLoginChannel(buf, datalen, info);
 			break;
-			
+
 		case ICQ_DATA_CHAN:
 			handleDataChannel(buf, datalen);
 			break;
-			
+
 		case ICQ_ERROR_CHAN:
 			handleErrorChannel(buf, datalen);
 			break;
-			
+
 		case ICQ_CLOSE_CHAN:
 			handleCloseChannel(buf, datalen);
 			return 0;
-			
+
 		case ICQ_PING_CHAN:
 			handlePingChannel(buf, datalen);
 			return 0;
-			
+
 		default:
 			Netlib_Logf(ghServerNetlibUser, "Warning: Unhandled Server FLAP Channel: Channel %u, Seq %u, Length %u bytes", channel, sequence, datalen);
 			break;
 		}
-		
+
 		/* Increase pointers so we can check for more FLAPs */
 		buf += datalen;
 		len -= (datalen + 6);
 		bytesUsed += (datalen + 6);
 	}
-	
+
 	return bytesUsed;
 }
 
@@ -336,12 +336,12 @@ void sendServPacket(icq_packet* pPacket)
 			if (gnCurrentStatus != ID_STATUS_OFFLINE)
 			{
 				int oldStatus = gnCurrentStatus;
-				
+
 				gnCurrentStatus = ID_STATUS_OFFLINE;
 				ProtoBroadcastAck(gpszICQProtoName, NULL, ACKTYPE_STATUS, ACKRESULT_SUCCESS,
 					(HANDLE)oldStatus, gnCurrentStatus);
 			}
-			
+
 		}
 
 	}
@@ -352,11 +352,11 @@ void sendServPacket(icq_packet* pPacket)
 
 	}
 
-	
+
 	LeaveCriticalSection(&localSeqMutex);
 
 
-	SAFE_FREE(pPacket->pData);
+	SAFE_FREE(&pPacket->pData);
 
 }
 
@@ -369,9 +369,9 @@ void icq_login(const char* szPassword)
 	serverthread_start_info* stsi;
 	DWORD dwUin;
 
-	
-	dwUin = DBGetContactSettingDword(NULL, gpszICQProtoName, UNIQUEIDSETTING, 0);				
-	stsi = (serverthread_start_info*)malloc(sizeof(serverthread_start_info));
+
+	dwUin = DBGetContactSettingDword(NULL, gpszICQProtoName, UNIQUEIDSETTING, 0);
+	stsi = (serverthread_start_info*)calloc(sizeof(serverthread_start_info), 1);
 	stsi->nloc.cbSize = sizeof(NETLIBOPENCONNECTION);
 	stsi->nloc.flags = 0;
 
@@ -382,7 +382,7 @@ void icq_login(const char* szPassword)
 	else
 		stsi->nloc.szHost = _strdup(dbvServer.pszVal);
 
-	// Server port			
+	// Server port
 	stsi->nloc.wPort = (WORD)DBGetContactSettingWord(NULL, gpszICQProtoName, "OscarPort", DEFAULT_SERVER_PORT);
 	if (stsi->nloc.wPort == 0)
 		stsi->nloc.wPort = RandRange(1024, 65535);
