67a4fecdb167a4adb3e2db173ec6cee8207a766e
Sam Kothari <egodust@miranda-im.org>
2005-01-28 21:13:29 +0000

    code that selects best rowheight uses the minheight of 16, this can be set lower if desired though
    git-svn-id: http://miranda.googlecode.com/svn/trunk@732 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist_nicer/clcopts.c
+++ miranda/plugins/clist_nicer/clcopts.c
@@ -840,7 +840,7 @@ static BOOL CALLBACK DlgProcClcTextOpts(HWND hwndDlg, UINT msg, WPARAM wParam, L
 			break;
 		case M_REDOROWHEIGHT:	//recalculate the minimum feasible row height
 		{	int i;
-			int minHeight=1;//GetSystemMetrics(SM_CYSMICON) + 1;
+			int minHeight=16;//GetSystemMetrics(SM_CYSMICON) + 1;
 			for(i=0;i<=FONTID_MAX;i++)
 				if(fontSettings[i].size>minHeight) minHeight=fontSettings[i].size;
 			i=SendDlgItemMessage(hwndDlg,IDC_ROWHEIGHTSPIN,UDM_GETPOS,0,0);
