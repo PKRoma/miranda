8ba9bf09741ccc8d36c375aca729bd16301bd22a
Sam Kothari <egodust@miranda-im.org>
2005-01-29 15:55:26 +0000

    ver bump to .11
    git-svn-id: http://miranda.googlecode.com/svn/trunk@741 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist_nicer/init.c
+++ miranda/plugins/clist_nicer/init.c
@@ -31,7 +31,7 @@ extern HWND hwndContactList;
 PLUGININFO pluginInfo = {
 	sizeof(PLUGININFO),
 	"Nicer contact list",
-	PLUGIN_MAKE_VERSION(0,3,4,10),
+	PLUGIN_MAKE_VERSION(0,3,4,11),
 	"Display contacts, event notifications, protocol status",
 	"Pixel, egoDust, cyreve",
 	"",
