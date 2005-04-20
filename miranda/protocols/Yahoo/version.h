7e45e8a39f7ec3f3e2eaec6bb9421615aee1bf17
Gennady Feldman <gena01@users.sourceforge.net>
2005-04-20 01:28:18 +0000

    Forgot to update version.
    git-svn-id: http://miranda.googlecode.com/svn/trunk@1199 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/Yahoo/version.h
+++ miranda/protocols/Yahoo/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,3,0,6
-#define __VERSION_STRING            "0.3.0.6"
-#define __VERSION_DWORD             0x00030006
+#define __FILEVERSION_STRING        0,3,0,7
+#define __VERSION_STRING            "0.3.0.7"
+#define __VERSION_DWORD             0x00030007
