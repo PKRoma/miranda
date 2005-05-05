00f1edc16a891ecbc16848f1d1f03bbd68b60dc0
Tristan Van de Vreede <tristan_vdv@users.sourceforge.net>
2005-05-05 12:42:05 +0000

    This commit was manufactured by cvs2svn to create tag 'aimtoc_1_2_0_0'.
    git-svn-id: http://miranda.googlecode.com/svn/tags/aimtoc_1_2_0_0@1366 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda-expired/miranda-icq007/miranda32/docking.h
+++
@@ -1,9 +0,0 @@
-
-
-#define WM_DOCKCB (WM_USER+121)
-
-void Docking_Init(HWND hwnd);
-void Docking_UpdateSize(HWND hwnd);
-void Docking_Kill(HWND hwnd);
-void Docking_ResizeWndToDock();
-void Docking_UpdateMenu(HWND hwnd);
--- miranda/protocols/JabberG/version.h
+++
@@ -1,3 +0,0 @@
-#define __FILEVERSION_STRING        0,4,0,2
-#define __VERSION_STRING            "0.4.0.2"
-#define __VERSION_DWORD             0x00040002
