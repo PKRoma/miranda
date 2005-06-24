c0d8635f672f41323c93bfa497ac236fb1c90179
Josef Kučera <jokusoftware@users.sourceforge.net>
2005-06-24 01:41:52 +0000

    added internal API for xtraz messaging support
    git-svn-id: http://miranda.googlecode.com/svn/trunk@1648 83df23c2-51fd-11de-bb24-2d5e9b4af41d

---
+++ miranda/protocols/IcqOscarJ/icq_xtraz.c
@@ -0,0 +1,322 @@
+// ---------------------------------------------------------------------------80
+//                ICQ plugin for Miranda Instant Messenger
+//                ________________________________________
+// 
+// Copyright © 2000,2001 Richard Hughes, Roland Rabien, Tristan Van de Vreede
+// Copyright © 2001,2002 Jon Keating, Richard Hughes
+// Copyright © 2002,2003,2004 Martin Öberg, Sam Kothari, Robert Rainwater
+// Copyright © 2004,2005 Joe Kucera
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// -----------------------------------------------------------------------------
+//
+// File name      : $Source$
+// Revision       : $Revision$
+// Last change on : $Date$
+// Last change by : $Author$
+//
+// DESCRIPTION:
+//
+//  Internal Xtraz API
+//
+// -----------------------------------------------------------------------------
+
+#include "icqoscar.h"
+
+
+void handleXtrazNotify(DWORD dwUin, DWORD dwMID, DWORD dwMID2, WORD wCookie, char* szMsg, int nMsgLen, BOOL bThruDC)
+{
+  char *szWork, *szEnd, *szNotify, *szQuery;
+  int nNotifyLen, nQueryLen;
+
+  szNotify = strstr(szMsg, "<NOTIFY>");
+  szQuery = strstr(szMsg, "<QUERY>");
+
+  if (szNotify && szQuery)
+  { // valid request
+    szNotify += 8;
+    szQuery += 7;
+    szEnd = strstr(szMsg, "</NOTIFY>");
+    if (!szEnd) szEnd = szMsg + nMsgLen;
+    nNotifyLen = (szEnd - szNotify);
+    szEnd = strstr(szMsg, "</QUERY>");
+    if (!szEnd) szEnd = szNotify;
+    szNotify = DemangleXml(szNotify, nNotifyLen);
+    nQueryLen = (szEnd - szQuery);
+    szQuery = DemangleXml(szQuery, nQueryLen);
+    szWork = strstr(szQuery, "<PluginID>");
+    szEnd = strstr(szQuery, "</PluginID>");
+#ifdef _DEBUG
+    Netlib_Logf(ghServerNetlibUser, "Query: %s", szQuery);
+    Netlib_Logf(ghServerNetlibUser, "Notify: %s", szNotify);
+#endif
+    if (szWork && szEnd)
+    { // this is our plugin
+      szWork += 10;
+      *szEnd = '\0';
+
+      if (!stricmp(szWork, "srvMng") && strstr(szNotify, "AwayStat"))
+      {
+        char* szSender = strstr(szNotify, "<senderId>");
+        char* szEndSend = strstr(szNotify, "</senderId>");
+
+        if (szSender && szEndSend)
+        {
+          szSender += 10;
+          *szEndSend = '\0';
+
+          if ((DWORD)atoi(szSender) == dwUin)
+          {
+            char *szResponse;
+            int nResponseLen;
+            char *szXName, *szXMsg;
+            BYTE dwXId = DBGetContactSettingByte(NULL, gpszICQProtoName, "XStatusId", 0);
+            DBVARIANT dbv;
+
+            if (dwXId)
+            {
+              char *szUtf;
+
+              if (!DBGetContactSetting(NULL, gpszICQProtoName, "XStatusName", &dbv))
+                szXName = dbv.pszVal;
+              else 
+                szXName = "";
+              // Prepare utf-8
+              if (strlen(szXName) > 0)
+              {
+                int nResult;
+
+                nResult = utf8_encode(szXName, &szUtf);
+              }
+              else
+                szUtf = strdup(szXName);
+              DBFreeVariant(&dbv);
+              szXName = szUtf;
+
+              if (!DBGetContactSetting(NULL, gpszICQProtoName, "XStatusMsg", &dbv))
+                szXMsg = dbv.pszVal;
+              else
+                szXMsg = "";
+              // Prepare utf-8
+              if (strlen(szXMsg) > 0)
+              {
+                int nResult;
+
+                nResult = utf8_encode(szXMsg, &szUtf);
+              }
+              else
+                szUtf = strdup(szXMsg);
+              DBFreeVariant(&dbv);
+              szXMsg = szUtf;
+              
+              nResponseLen = 212 + strlennull(szXName) + strlennull(szXMsg) + UINMAXLEN + 2;
+              szResponse = (char*)malloc(nResponseLen + 1);
+              // send response
+              nResponseLen = mir_snprintf(szResponse, nResponseLen, 
+                "<ret event='OnRemoteNotification'>"
+                "<srv><id>cAwaySrv</id>"
+                "<val srv_id='cAwaySrv'><Root>"
+                "<CASXtraSetAwayMessage></CASXtraSetAwayMessage>"
+                "<uin>%d</uin>"
+                "<index>%d</index>"
+                "<title>%s</title>"
+                "<desc>%s</desc></Root></val></srv></ret>",
+                dwLocalUIN, dwXId, szXName, szXMsg);
+
+              SAFE_FREE(&szXName);
+              SAFE_FREE(&szXMsg);
+
+              if (gbXStatusEnabled)
+                SendXtrazNotifyResponse(dwUin, dwMID, dwMID2, wCookie, szResponse, nResponseLen, bThruDC);
+              else
+                Netlib_Logf(ghServerNetlibUser, "XStatus Disabled");
+
+              SAFE_FREE(&szResponse);
+            }
+            else
+              Netlib_Logf(ghServerNetlibUser, "Error: We are not in XStatus, skipping");
+          }
+          else
+            Netlib_Logf(ghServerNetlibUser, "Error: Invalid sender information");
+        }
+        else
+          Netlib_Logf(ghServerNetlibUser, "Error: Missing sender information");
+      }
+      else
+        Netlib_Logf(ghServerNetlibUser, "Error: Unknown plugin \"%s\" in Xtraz message", szWork);
+    }
+    else 
+      Netlib_Logf(ghServerNetlibUser, "Error: Missing PluginID in Xtraz message");
+
+    SAFE_FREE(&szNotify);
+    SAFE_FREE(&szQuery);
+  }
+  else 
+    Netlib_Logf(ghServerNetlibUser, "Error: Invalid Xtraz Notify message");
+}
+
+
+void handleXtrazNotifyResponse(DWORD dwUin, HANDLE hContact, char* szMsg, int nMsgLen)
+{
+  char *szRes, *szEnd;
+  int nResLen;
+
+#ifdef _DEBUG
+  Netlib_Logf(ghServerNetlibUser, "Received Xtraz Notify Response");
+#endif
+
+  szRes = strstr(szMsg, "<RES>");
+  szEnd = strstr(szMsg, "</RES>");
+
+  if (szRes && szEnd)
+  { // valid response
+    char *szNode, *szWork;
+
+    szRes += 5;
+    nResLen = szEnd - szRes;
+
+    szRes = DemangleXml(szRes, nResLen);
+
+    szNode = strstr(szRes, "<val srv_id='");
+    if (szNode) szEnd = strstr(szNode, ">"); else szEnd = NULL;
+
+    if (szNode && szEnd)
+    {
+      *(szEnd-1) = '\0';
+      szNode += 13;
+      szWork = szEnd + 1;
+
+      if (!stricmp(szNode, "cAwaySrv"))
+      {
+        szNode = strstr(szWork, "<uin>");
+        szEnd = strstr(szWork, "</uin>");
+
+        if (szNode && szEnd)
+        {
+          szNode += 5;
+          *szEnd = '\0';
+
+          if ((DWORD)atoi(szNode) == dwUin)
+          {
+            *szEnd = ' ';
+            szNode = strstr(szWork, "<index>");
+            szEnd = strstr(szWork, "</index>");
+
+            if (szNode && szEnd)
+            {
+              szNode += 7;
+              *szEnd = '\0';
+              if (atoi(szNode) != DBGetContactSettingByte(hContact, gpszICQProtoName, "XStatusId", 0))
+              { // this is strange - but go on
+                Netlib_Logf(ghServerNetlibUser, "Warning: XStatusIds do not match!");
+              }
+              *szEnd = ' ';
+            }
+            szNode = strstr(szWork, "<title>");
+            szEnd = strstr(szWork, "</title>");
+
+            if (szNode && szEnd)
+            { // we got XStatus title, save it
+              char *szData;
+
+              szNode += 7;
+              *szEnd = '\0';
+              if (!utf8_decode(szNode, &szData)) szData = strdup(szNode);
+              DBWriteContactSettingString(hContact, gpszICQProtoName, "XStatusName", szData);
+              SAFE_FREE(&szData);
+              *szEnd = ' ';
+            }
+            szNode = strstr(szWork, "<desc>");
+            szEnd = strstr(szWork, "</desc>");
+
+            if (szNode && szEnd)
+            { // we got XStatus mode msg, save it
+              char *szData;
+
+              szNode += 6;
+              *szEnd = '\0';
+              if (!utf8_decode(szNode, &szData)) szData = strdup(szNode);
+              DBWriteContactSettingString(hContact, gpszICQProtoName, "XStatusMsg", szData);
+              SAFE_FREE(&szData);
+            }
+          }
+          else
+            Netlib_Logf(ghServerNetlibUser, "Error: Invalid sender information");
+        }
+        else
+          Netlib_Logf(ghServerNetlibUser, "Error: Missing sender information");
+      }
+      else
+        Netlib_Logf(ghServerNetlibUser, "Error: Unknown serverId \"%s\" in Xtraz response", szNode);
+    }
+    else
+      Netlib_Logf(ghServerNetlibUser, "Error: Missing serverId in Xtraz response");
+
+    SAFE_FREE(&szRes);
+  }
+  else
+    Netlib_Logf(ghServerNetlibUser, "Error: Invalid Xtraz Notify response");
+}
+
+
+void SendXtrazNotifyRequest(HANDLE hContact, char* szQuery, char* szNotify)
+{
+  char *szQueryBody = MangleXml(szQuery, strlen(szQuery));
+  char *szNotifyBody = MangleXml(szNotify, strlen(szNotify));
+  DWORD dwUin = DBGetContactSettingDword(hContact, gpszICQProtoName, UNIQUEIDSETTING, 0);
+  int nBodyLen = strlennull(szQueryBody) + strlennull(szNotifyBody) + 41;
+  char *szBody = (char*)malloc(nBodyLen);
+  DWORD dwCookie;
+  message_cookie_data* pCookieData;
+
+  nBodyLen = mir_snprintf(szBody, nBodyLen, "<N><QUERY>%s</QUERY><NOTIFY>%s</NOTIFY></N>", szQueryBody, szNotifyBody);
+  SAFE_FREE(&szQueryBody);
+  SAFE_FREE(&szNotifyBody);
+
+  // Set up the ack type
+  pCookieData = malloc(sizeof(message_cookie_data));
+  pCookieData->bMessageType = MTYPE_PLUGIN; // we do not use this
+  pCookieData->nAckType = ACKTYPE_CLIENT;
+  dwCookie = AllocateCookie(0, dwUin, (void*)pCookieData);
+
+  // have we a open DC, send through that
+  if (gbDCMsgEnabled && IsDirectConnectionOpen(hContact, DIRECTCONN_STANDARD))
+    icq_sendXtrazRequestDirect(dwUin, hContact, dwCookie, szBody, nBodyLen, MGTYPE_SCRIPT_NOTIFY);
+  else
+    icq_sendXtrazRequestServ(dwUin, dwCookie, szBody, nBodyLen, MTYPE_SCRIPT_NOTIFY);
+
+  SAFE_FREE(&szBody);
+}
+
+
+void SendXtrazNotifyResponse(DWORD dwUin, DWORD dwMID, DWORD dwMID2, WORD wCookie, char* szResponse, int nResponseLen, BOOL bThruDC)
+{
+  char *szResBody = MangleXml(szResponse, nResponseLen);
+  int nBodyLen = strlennull(szResBody) + 21;
+  char *szBody = (char*)malloc(nBodyLen);
+  HANDLE hContact = HContactFromUIN(dwUin, 0);
+
+  nBodyLen = mir_snprintf(szBody, nBodyLen, "<NR><RES>%s</RES></NR>", szResBody);
+  SAFE_FREE(&szResBody);
+
+  // Was request received thru DC and have we a open DC, send through that
+  if (bThruDC && IsDirectConnectionOpen(hContact, DIRECTCONN_STANDARD))
+    icq_sendXtrazResponseDirect(dwUin, hContact, dwMID, dwMID2, wCookie, szBody, nBodyLen, MGTYPE_SCRIPT_NOTIFY);
+  else
+    icq_sendXtrazResponseServ(dwUin, dwMID, dwMID2, wCookie, szBody, nBodyLen, MTYPE_SCRIPT_NOTIFY);
+
+  SAFE_FREE(&szBody);
+}
