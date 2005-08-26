08b9d67f476619503808a1948ada2f284ef312b8
George Hazan <ghazan@miranda-im.org>
2005-08-26 21:24:39 +0000

    version bump
    git-svn-id: http://miranda.googlecode.com/svn/trunk@1811 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/version.h
+++ miranda/protocols/MSN/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,4,1,1
-#define __VERSION_STRING            "0.4.1.1"
-#define __VERSION_DWORD             0x00040101
+#define __FILEVERSION_STRING        0,4,1,2
+#define __VERSION_STRING            "0.4.1.2"
+#define __VERSION_DWORD             0x00040102
