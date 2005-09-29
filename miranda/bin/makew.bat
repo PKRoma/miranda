1c182855aeca351ededb7844a56bded57693a7bc
George Hazan <ghazan@miranda-im.org>
2005-09-29 18:47:31 +0000

    feature request 0001497: Add build # to 'about' dialog
    git-svn-id: http://miranda.googlecode.com/svn/trunk@1990 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/bin/makew.bat
+++ miranda/bin/makew.bat
@@ -1,5 +1,7 @@
 @echo off
 
+for /F "tokens=1,2 delims= " %%i in (build.no) do call :WriteVer %%i %%j
+
 rem ---------------------------------------------------------------------------
 rem Main modules
 rem ---------------------------------------------------------------------------
@@ -86,6 +88,56 @@ copy ..\release\Plugins\Yahoo.dll    Plugins
 
 for /F "tokens=1,2 delims= " %%i in (..\build.no) do call :Pack %%i %%j
 exit
+
+:WriteVer
+echo #ifndef _MAC >..\src\version.rc
+echo ///////////////////////////////////////////////////////////////////////////// >>..\src\version.rc
+echo //                                                                            >>..\src\version.rc
+echo // Version                                                                    >>..\src\version.rc
+echo //                                                                            >>..\src\version.rc
+echo.                                                                              >>..\src\version.rc
+echo VS_VERSION_INFO VERSIONINFO                                                   >>..\src\version.rc
+echo  FILEVERSION 0,4,3,%2                                                         >>..\src\version.rc
+echo  PRODUCTVERSION 0,4,3,%2                                                      >>..\src\version.rc
+echo  FILEFLAGSMASK 0x3fL                                                          >>..\src\version.rc
+echo #ifdef _DEBUG                                                                 >>..\src\version.rc
+echo  FILEFLAGS 0x1L                                                               >>..\src\version.rc
+echo #else                                                                         >>..\src\version.rc
+echo  FILEFLAGS 0x0L                                                               >>..\src\version.rc
+echo #endif                                                                        >>..\src\version.rc
+echo  FILEOS 0x40004L                                                              >>..\src\version.rc
+echo  FILETYPE 0x1L                                                                >>..\src\version.rc
+echo  FILESUBTYPE 0x0L                                                             >>..\src\version.rc
+echo BEGIN                                                                         >>..\src\version.rc
+echo     BLOCK "StringFileInfo"                                                    >>..\src\version.rc
+echo     BEGIN                                                                     >>..\src\version.rc
+echo         BLOCK "000004b0"                                                      >>..\src\version.rc
+echo         BEGIN                                                                 >>..\src\version.rc
+echo             VALUE "Comments", "Licensed under the terms of the GNU General Public License\0" >>..\src\version.rc
+echo             VALUE "CompanyName", " \0"                                        >>..\src\version.rc
+echo             VALUE "FileDescription", "Miranda IM\0"                           >>..\src\version.rc
+echo             VALUE "FileVersion", "0.4.3 alpha bld.%2\0"                       >>..\src\version.rc
+echo             VALUE "InternalName", "miranda32\0"                               >>..\src\version.rc
+echo             VALUE "LegalCopyright", "Copyright c 2000-2005 Richard Hughes, Roland Rabien, Tristan Van de Vreede, Martin –berg, Robert Rainwater, Sam Kothari and Lyon Lim\0" >>..\src\version.rc
+echo             VALUE "LegalTrademarks", "\0"                                     >>..\src\version.rc
+echo             VALUE "OriginalFilename", "miranda32.exe\0"                       >>..\src\version.rc
+echo             VALUE "PrivateBuild", "\0"                                        >>..\src\version.rc
+echo             VALUE "ProductName", "Miranda IM\0"                               >>..\src\version.rc
+echo             VALUE "ProductVersion", "0.4.3 alpha bld.%2\0"                    >>..\src\version.rc
+echo             VALUE "SpecialBuild", "\0"                                        >>..\src\version.rc
+echo         END                                                                   >>..\src\version.rc
+echo     END                                                                       >>..\src\version.rc
+echo     BLOCK "VarFileInfo"                                                       >>..\src\version.rc
+echo     BEGIN                                                                     >>..\src\version.rc
+echo         VALUE "Translation", 0x0, 1200                                        >>..\src\version.rc
+echo     END                                                                       >>..\src\version.rc
+echo END                                                                           >>..\src\version.rc
+echo.                                                                              >>..\src\version.rc
+echo #endif    // !_MAC                                                            >>..\src\version.rc
+echo.                                                                              >>..\src\version.rc
+echo #endif    // English (U.S.) resources                                         >>..\src\version.rc
+echo.                                                                              >>..\src\version.rc
+goto :eof
 
 :Pack
 del %Temp%\miranda-v%1a%2w.zip
