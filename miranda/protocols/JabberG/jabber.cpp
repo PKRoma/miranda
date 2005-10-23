766cf6e7ec057f36359b240e0618ebde1fcbb1ac
George Hazan <ghazan@miranda-im.org>
2005-10-23 18:50:53 +0000

    *** empty log message ***
    git-svn-id: http://miranda.googlecode.com/svn/trunk@2069 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/jabber.cpp
+++ miranda/protocols/JabberG/jabber.cpp
@@ -18,6 +18,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+File name      : $Source$
+Revision       : $Revision$
+Last change on : $Date$
+Last change by : $Author$
+
 */
 
 #include "jabber.h"
@@ -164,9 +169,9 @@ int JabberGcMenuHook( WPARAM, LPARAM );
 int JabberGcInit( WPARAM, LPARAM );
 
 static COLORREF crCols[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-HANDLE hChatEvent = NULL, 
-       hChatMenu = NULL, 
-		 hInitChat = NULL, 
+HANDLE hChatEvent = NULL,
+       hChatMenu = NULL,
+		 hInitChat = NULL,
 		 hEvInitChat = NULL,
 		 hEvModulesLoaded = NULL,
 		 hEvOptInit = NULL,
