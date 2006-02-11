4718f0a20016e10ba83c1cc290dfdb0b59befab4
Tristan Van de Vreede <tristan_vdv@users.sourceforge.net>
2006-02-11 21:42:00 +0000

    This commit was manufactured by cvs2svn to create tag 'miranda_v0_4_0_3'.
    git-svn-id: http://miranda.googlecode.com/svn/tags/miranda_v0_4_0_3@2584 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/version.h
+++
@@ -1,3 +0,0 @@
-#define __FILEVERSION_STRING        0,4,0,4
-#define __VERSION_STRING            "0.4.0.4"
-#define __VERSION_DWORD             0x00040004
--- miranda/protocols/Yahoo/version.h
+++
@@ -1,3 +0,0 @@
-#define __FILEVERSION_STRING        0,3,0,8
-#define __VERSION_STRING            "0.3.0.8"
-#define __VERSION_DWORD             0x00030008
