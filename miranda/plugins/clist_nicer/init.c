7c92458b2bbd32ea20b12e9f62347211423bd62f
Alex Vie <nightwish2004@users.sourceforge.net>
2006-02-26 15:36:47 +0000

    version bump, changelog update
    git-svn-id: http://miranda.googlecode.com/svn/trunk@2636 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist_nicer/init.c
+++ miranda/plugins/clist_nicer/init.c
@@ -103,9 +103,9 @@ void RecalcScrollBar(HWND hwnd, struct ClcData *dat);
 
 PLUGININFO pluginInfo = {
 #if defined(_UNICODE)
-	sizeof(PLUGININFO), "CList Nicer+ (Unicode)", PLUGIN_MAKE_VERSION(0, 6, 0, 5),
+	sizeof(PLUGININFO), "CList Nicer+ (Unicode)", PLUGIN_MAKE_VERSION(0, 6, 0, 6),
 #else
-	sizeof(PLUGININFO), "CList Nicer+", PLUGIN_MAKE_VERSION(0, 6, 0, 5),
+	sizeof(PLUGININFO), "CList Nicer+", PLUGIN_MAKE_VERSION(0, 6, 0, 6),
 #endif
 		"Display contacts, event notifications, protocol status",
 		"Pixel, egoDust, cyreve, Nightwish", "", "Copyright 2000-2005 Miranda-IM project", "http://www.miranda-im.org",
