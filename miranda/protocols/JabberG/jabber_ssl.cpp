ac19b82cb9faab026a5972e9f66b16d6e0cf8b67
George Hazan <ghazan@miranda-im.org>
2006-05-12 20:13:35 +0000

    minor comments changes - copyrights etc
    git-svn-id: http://miranda.googlecode.com/svn/trunk@2843 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/jabber_ssl.cpp
+++ miranda/protocols/JabberG/jabber_ssl.cpp
@@ -2,9 +2,9 @@
 
 Jabber Protocol Plugin for Miranda IM
 Copyright ( C ) 2002-04  Santithorn Bunchua
-Copyright ( C ) 2005     George Hazan
+Copyright ( C ) 2005-06  George Hazan
 
-This program is mir_free software; you can redistribute it and/or
+This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or ( at your option ) any later version.
--- miranda/protocols/JabberG/jabber_std.cpp
+++ miranda/protocols/JabberG/jabber_std.cpp
@@ -6,7 +6,7 @@ Copyright ( c ) 2002-3 Richard Hughes ( original version ).
 Miranda IM: the mir_free icq client for MS Windows
 Copyright ( C ) 2000-2002 Richard Hughes, Roland Rabien & Tristan Van de Vreede
 
-This program is mir_free software; you can redistribute it and/or
+This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or ( at your option ) any later version.
