25b7757753d09d58a071de086b0fb9d8ba7b21cf
Alex Vie <nightwish2004@users.sourceforge.net>
2006-07-05 19:15:49 +0000

    + version bump + fixed problem with visibility + ignore dialog
    git-svn-id: http://miranda.googlecode.com/svn/branches/0_5_x@3278 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist_nicer/init.c
+++ miranda/plugins/clist_nicer/init.c
@@ -109,9 +109,9 @@ void RecalcScrollBar(HWND hwnd, struct ClcData *dat);
 
 PLUGININFO pluginInfo = {
 #if defined(_UNICODE)
-	sizeof(PLUGININFO), "CList Nicer+ (Unicode)", PLUGIN_MAKE_VERSION(0, 7, 0, 0),
+	sizeof(PLUGININFO), "CList Nicer+ (Unicode)", PLUGIN_MAKE_VERSION(0, 7, 0, 1),
 #else
-	sizeof(PLUGININFO), "CList Nicer+", PLUGIN_MAKE_VERSION(0, 7, 0, 0),
+	sizeof(PLUGININFO), "CList Nicer+", PLUGIN_MAKE_VERSION(0, 7, 0, 1),
 #endif
 		"Display contacts, event notifications, protocol status",
 		"Pixel, egoDust, cyreve, Nightwish", "", "Copyright 2000-2006 Miranda-IM project", "http://www.miranda-im.org",
