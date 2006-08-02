468183d722e6f97ebd22a4dcbd9318b9b57aa486
Aaron Landwehr <snaphat@users.sourceforge.net>
2006-08-02 04:57:42 +0000

    -changed the vc7 proj files -faithhealer modded the resource.h
    git-svn-id: http://miranda.googlecode.com/svn/trunk@3426 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/AimOscar/resource.h
+++ miranda/protocols/AimOscar/resource.h
@@ -6,28 +6,52 @@
 #define IDD_AIMACCOUNT                  7
 #define IDD_AIM                         8
 #define IDD_IDLE                        9
-#define IDI_ADD                         10
-#define IDI_AWAY                        12
-#define IDI_IDLE                        13
-#define IDI_PROFILE                     14
-#define IDI_MAIL                        15
-#define IDI_BOLD                        18
-#define IDI_NBOLD                       19
-#define IDI_NITALIC                     20
-#define IDI_ITALIC                      21
-#define IDI_NUNDERLINE                  22
-#define IDI_UNDERLINE                   23
-#define IDI_NINCREASEFONT               25
-#define IDI_NDECREASEFONT               26
-#define IDI_FOREGROUNDCOLOR             28
-#define IDI_BACKGROUNDCOLOR             29
-#define IDI_NSUBSCRIPT                  30
-#define IDI_NSUPERSCRIPT                31
-#define IDI_NNORMALSCRIPT               32
-#define IDI_NORMALSCRIPT                33
-#define IDI_SUBSCRIPT                   34
-#define IDI_ICON4                       35
-#define IDI_SUPERSCRIPT                 35
+
+#define IDI_AIM                         10
+#define IDI_ICQ                         11
+#define IDI_ADD                         12
+#define IDI_PROFILE                     13
+#define IDI_MAIL                        14
+
+
+#define IDI_HIPTOP                      21
+#define IDI_BOT                         22
+#define IDI_ADMIN                       23
+#define IDI_CONFIRMED                   24
+#define IDI_UNCONFIRMED                 25
+#define IDI_AWAY                        26
+#define IDI_IDLE                        27
+#define IDI_AOL                         28
+
+
+#define IDI_FOREGROUNDCOLOR             31
+#define IDI_BACKGROUNDCOLOR             32
+
+#define IDI_BOLD                        41
+#define IDI_NBOLD                       42
+
+#define IDI_ITALIC                      43
+#define IDI_NITALIC                     44
+
+#define IDI_UNDERLINE                   45
+#define IDI_NUNDERLINE                  46
+
+#define IDI_NINCREASEFONT               51
+#define IDI_NDECREASEFONT               52
+
+#define IDI_SUBSCRIPT                   53
+#define IDI_NSUBSCRIPT                  54
+
+#define IDI_SUPERSCRIPT                 55
+#define IDI_NSUPERSCRIPT                56
+
+#define IDI_NORMALSCRIPT                57
+#define IDI_NNORMALSCRIPT               58
+
+
+#define IDI_ICON4                       61
+
+
 #define IDC_DETAILS                     125
 #define IDC_OPTIONS                     126
 #define IDC_EXPERT                      127
@@ -46,14 +70,7 @@
 #define IDC_BACKGROUNDCOLOR             138
 #define IDC_TYPEFACE                    139
 #define IDC_FONTSIZE                    141
-#define IDI_AIM                         1000
-#define IDI_ICQ                         1001
-#define IDI_AOL                         1002
-#define IDI_HIPTOP                      1003
-#define IDI_BOT                         1004
-#define IDI_ADMIN                       1005
-#define IDI_CONFIRMED                   1006
-#define IDI_UNCONFIRMED                 1007
+
 #define IDC_SN                          1030
 #define IDC_NK                          1040
 #define IDC_PW                          1050
