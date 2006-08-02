28093f490e1f0ba365fca414f1e5d18cb4538611
Robert Rainwater <rainwater@miranda-im.org>
2006-08-02 18:32:05 +0000

    Mass copyright year update
    git-svn-id: http://miranda.googlecode.com/svn/trunk@3431 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/include/m_addcontact.h
+++ miranda/include/m_addcontact.h
@@ -1,6 +1,10 @@
 /*
-Miranda ICQ: the free icq client for MS Windows 
-Copyright (C) 2000-4  Richard Hughes, Roland Rabien & Tristan Van de Vreede
+
+Miranda IM: the free IM client for Microsoft* Windows*
+
+Copyright 2000-2006 Miranda ICQ/IM project, 
+all portions of this codebase are copyrighted to the people 
+listed in contributors.txt.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
