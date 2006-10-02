c99690fad0f8bce6f669381581d0b78559e74200
George Hazan <ghazan@miranda-im.org>
2006-10-02 07:57:20 +0000

    fix for non-sorted lists
    git-svn-id: http://miranda.googlecode.com/svn/trunk@3937 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/database/dblists.c
+++ miranda/src/modules/database/dblists.c
@@ -119,7 +119,7 @@ int List_Insert( SortedList* p_list, void* p_value, int p_index)
 
 int List_InsertPtr( SortedList* list, void* p )
 {
-	int idx;
+	int idx = list->realCount;
 	List_GetIndex( list, p, &idx );
 	return List_Insert( list, p, idx );
 }
