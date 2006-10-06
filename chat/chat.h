d75b2366d3ed06e6b07fc61cb3ad0073652fa5d8
Piotr Piastucki <leech.miranda@gmail.com>
2006-10-06 07:01:06 +0000

    * malloc -> mir_alloc migration
    git-svn-id: http://miranda.googlecode.com/svn/trunk/miranda/plugins/Scriver@3986 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- chat/chat.h
+++ chat/chat.h
@@ -540,31 +540,10 @@ TCHAR*        DoRtfToTags( char* pszRtfText, SESSION_INFO* si);
 
 //////////////////////////////////////////////////////////////////////////////////
 
-extern struct MM_INTERFACE memoryManagerInterface;
-#define mir_alloc(n) memoryManagerInterface.mmi_malloc(n)
-#define mir_free(ptr) memoryManagerInterface.mmi_free(ptr)
-#define mir_realloc(ptr,size) memoryManagerInterface.mmi_realloc(ptr,size)
-
 #if defined( _UNICODE )
 	#define TCHAR_STR_PARAM "%S"
 #else
 	#define TCHAR_STR_PARAM "%s"
-#endif
-
-static __inline char * mir_strdup(const char *src)
-{
-	return (src == NULL) ? NULL : strcpy(( char* )mir_alloc( strlen(src)+1 ), src );
-}
-
-static __inline WCHAR* mir_wstrdup(const WCHAR *src)
-{
-	return (src == NULL) ? NULL : wcscpy(( WCHAR* )mir_alloc(( wcslen(src)+1 )*sizeof( WCHAR )), src );
-}
-
-#if defined( _UNICODE )
-	#define mir_tstrdup mir_wstrdup
-#else
-	#define mir_tstrdup mir_strdup
 #endif
 
 char*  t2a( const TCHAR* str );
