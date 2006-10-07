5434dc85bc3b331dc6b079cfea78e8f7ce5971e3
Gennady Feldman <gena01@users.sourceforge.net>
2006-10-07 03:01:48 +0000

    Updating version number on stable. In preparation for the next/new stable nightlies/releases...
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@3992 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/Yahoo/version.h
+++ miranda/protocols/Yahoo/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,3,0,9
-#define __VERSION_STRING            "0.3.0.9"
-#define __VERSION_DWORD             0x00030009
+#define __FILEVERSION_STRING        0,3,0,10
+#define __VERSION_STRING            "0.3.0.10"
+#define __VERSION_DWORD             0x0003000A
