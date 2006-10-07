378fba3f30ce0a94b95b8d847905189b0e2b9f98
Piotr Piastucki <leech.miranda@gmail.com>
2006-10-07 14:39:41 +0000

    gcc compatibility fix
    git-svn-id: http://miranda.googlecode.com/svn/trunk@3995 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/include/m_system.h
+++ miranda/include/m_system.h
@@ -36,7 +36,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #ifndef _MSC_VER
-#define __forceinline __inline
+#define __forceinline static FORCEINLINE
 #endif
 
 //miranda/system/modulesloaded
