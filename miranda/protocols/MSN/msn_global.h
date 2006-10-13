d7550ca47960d3376f182e703b59343e86fd1cf6
George Hazan <ghazan@miranda-im.org>
2006-10-13 09:14:57 +0000

    added missing file
    git-svn-id: http://miranda.googlecode.com/svn/trunk@4032 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_global.h
+++ miranda/protocols/MSN/msn_global.h
@@ -59,8 +59,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_system.h>
 #include <m_userinfo.h>
 #include <m_utils.h>
-#include <m_proto_listeningto.h>
 #include <win2k.h>
+
+#include "sdk/m_proto_listeningto.h"
 
 #include <m_database.h>
 #include <m_langpack.h>
