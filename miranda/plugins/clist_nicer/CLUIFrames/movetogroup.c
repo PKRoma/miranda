826f4c26afe17f314a39be4b7650cbd2cc497975
Alex Vie <nightwish2004@users.sourceforge.net>
2006-12-03 17:01:18 +0000

    + patch by Joe to fix move to group feature and uncommanded moving of contacts to other groups
    git-svn-id: http://miranda.googlecode.com/svn/trunk@4307 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist_nicer/CLUIFrames/movetogroup.c
+++ miranda/plugins/clist_nicer/CLUIFrames/movetogroup.c
@@ -1,10 +1,11 @@
 #include "../commonheaders.h"
 
-HANDLE hModulesLoaded,hOnCntMenuBuild;
-HANDLE prevmenu=0, hPriorityItem = 0, hFloatingItem = 0, hIgnoreItem = 0;
-extern char *DBGetString(HANDLE hContact,const char *szModule,const char *szSetting);
+HANDLE hOnCntMenuBuild;
+HANDLE hMoveToGroupItem=0, hPriorityItem = 0, hFloatingItem = 0, hIgnoreItem = 0;
+HANDLE *hGroupsItems = NULL;
+int nGroupsItems = 0, cbGroupsItems = 0;
 
-HWND hwndTopToolBar=0;
+extern char *DBGetString(HANDLE hContact,const char *szModule,const char *szSetting);
 
 //service
 //wparam - hcontact
@@ -12,185 +13,243 @@ HWND hwndTopToolBar=0;
 
 #define MTG_MOVE								"MoveToGroup/Move"
 
-static int AddGroupItem(int rootid,char *name,int pos,int poppos,WPARAM wParam)
+static HANDLE AddGroupItem(HANDLE hRoot,char *name,int pos,int param,int checked)
 {
-    CLISTMENUITEM mi={0};
+  CLISTMENUITEM mi={0};
 
+  mi.cbSize=sizeof(mi);
+  mi.pszPopupName=(char *)hRoot;
+  mi.popupPosition=param; // param to pszService - only with CMIF_CHILDPOPUP !!!!!!
+  mi.position=pos;
+  mi.pszName=name;
+  mi.flags=CMIF_CHILDPOPUP;
+  if (checked)
+    mi.flags |= CMIF_CHECKED;
+  mi.pszService=MTG_MOVE;
+  return (HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM,0, (LPARAM)&mi);
+}
 
-    mi.cbSize=sizeof(mi);
-    mi.hIcon=NULL;//LoadIcon(hInst,MAKEINTRESOURCE(IDI_MIRANDA));
-    mi.pszPopupName=(char *)rootid;
-    mi.popupPosition=poppos;
-    mi.position=pos;
-    mi.pszName=name;
-    mi.flags=CMIF_CHILDPOPUP;
-    mi.pszContactOwner=(char *)0;
-    mi.pszService=MTG_MOVE;
-    return(CallService(MS_CLIST_ADDCONTACTMENUITEM,wParam,(LPARAM)&mi));
+static void ModifyGroupItem(HANDLE hItem,char *name,int checked)
+{
+  CLISTMENUITEM mi={0};
 
+  mi.cbSize=sizeof(mi);
+  mi.pszName=name;
+  mi.flags=CMIM_NAME | CMIM_FLAGS;
+  if (checked)
+    mi.flags |= CMIF_CHECKED;
+  CallService(MS_CLIST_MODIFYMENUITEM, (WPARAM)hItem, (LPARAM)&mi);
 }
 
 static int OnContactMenuBuild(WPARAM wParam,LPARAM lParam)
 {
+  CLISTMENUITEM mi;
+  BYTE bSetting;
 
-    CLISTMENUITEM mi;
-    HANDLE menuid;
-    int i,grpid;
-    boolean grpexists;
-    char *grpname;
-    char intname[20];
-
-    if (prevmenu!=0) {
-        CallService(MS_CLIST_REMOVECONTACTMENUITEM, (WPARAM)prevmenu,(LPARAM)0);
-		CallService(MS_CLIST_REMOVECONTACTMENUITEM, (WPARAM)hPriorityItem, 0);
-		CallService(MS_CLIST_REMOVECONTACTMENUITEM, (WPARAM)hFloatingItem, 0);
-        CallService(MS_CLIST_REMOVECONTACTMENUITEM, (WPARAM)hIgnoreItem, 0);
-    }
-
-	if(DBGetContactSettingByte(0, "CList", "flt_enabled", 0)) {
-		memset(&mi,0,sizeof(mi));
+  // Menu Item - Floating Contact
+  bSetting = DBGetContactSettingByte(0, "CList", "flt_enabled", 0);
+  if (bSetting && !hFloatingItem)
+  {
+    memset(&mi,0,sizeof(mi));
 		mi.cbSize=sizeof(mi);
 		mi.position=200000;
 		mi.pszPopupName=(char *)-1;
 		mi.pszService="CList/SetContactFloating";
 		mi.pszName=Translate("&Floating Contact");
-		if(pcli) {
-			if(SendMessage(pcli->hwndContactTree, CLM_QUERYFLOATINGCONTACT, wParam, 0))
+		if (pcli) 
+    {
+			if (SendMessage(pcli->hwndContactTree, CLM_QUERYFLOATINGCONTACT, wParam, 0))
 				mi.flags=CMIF_CHECKED;
 		}
-		mi.pszContactOwner=(char *)0;
-		hFloatingItem = (HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM,wParam,(LPARAM)&mi);
+		hFloatingItem = (HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM, 0, (LPARAM)&mi);
 	}
+  else if (!bSetting && hFloatingItem)
+  {
+    CallService(MS_CLIST_REMOVECONTACTMENUITEM, (WPARAM)hFloatingItem, 0);
+    hFloatingItem = 0;
+  }
+  else
+  {
+    memset(&mi,0,sizeof(mi));
+		mi.cbSize=sizeof(mi);
+    mi.flags=CMIM_FLAGS;
+		if (pcli) 
+    {
+			if (SendMessage(pcli->hwndContactTree, CLM_QUERYFLOATINGCONTACT, wParam, 0))
+				mi.flags |= CMIF_CHECKED;
+		}
+    CallService(MS_CLIST_MODIFYMENUITEM, (WPARAM)hFloatingItem, (LPARAM)&mi);
+  }
 
-	memset(&mi,0,sizeof(mi));
+  // Menu Item - Priority Contact
+  if (!hPriorityItem)
+  {
+    memset(&mi,0,sizeof(mi));
     mi.cbSize=sizeof(mi);
     mi.position=200000;
     mi.pszPopupName=(char *)-1;
     mi.pszService="CList/SetContactPriority";
     mi.pszName=Translate("&Priority Contact");
-	if(pcli) {
-		if(SendMessage(pcli->hwndContactTree, CLM_QUERYPRIORITYCONTACT, wParam, 0))
-			mi.flags=CMIF_CHECKED;
-	}
-    mi.pszContactOwner=(char *)0;
-    hPriorityItem = (HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM,wParam,(LPARAM)&mi);
+    if (pcli) 
+    {
+      if (SendMessage(pcli->hwndContactTree, CLM_QUERYPRIORITYCONTACT, wParam, 0))
+        mi.flags = CMIF_CHECKED;
+    }
+    hPriorityItem = (HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM, 0, (LPARAM)&mi);
+  }
+  else
+  {
+    memset(&mi,0,sizeof(mi));
+		mi.cbSize=sizeof(mi);
+    mi.flags=CMIM_FLAGS;
+		if (pcli) 
+    {
+      if (SendMessage(pcli->hwndContactTree, CLM_QUERYPRIORITYCONTACT, wParam, 0))
+				mi.flags |= CMIF_CHECKED;
+		}
+    CallService(MS_CLIST_MODIFYMENUITEM, (WPARAM)hPriorityItem, (LPARAM)&mi);
+  }
 
+  if (!hIgnoreItem)
+  {
     memset(&mi,0,sizeof(mi));
     mi.cbSize=sizeof(mi);
     mi.position=200000;
     mi.pszPopupName=(char *)-1;
     mi.pszService="CList/SetContactIgnore";
     mi.pszName=Translate("&Visibility and ignore...");
-    mi.pszContactOwner=(char *)0;
-    hIgnoreItem = (HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM,wParam,(LPARAM)&mi);
+    hIgnoreItem = (HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM, 0, (LPARAM)&mi);
+  }
 
-
-    ZeroMemory(&mi,sizeof(mi));
+  if (!hMoveToGroupItem)
+  {
+    memset(&mi,0,sizeof(mi));
     mi.cbSize=sizeof(mi);
     mi.hIcon=NULL;//LoadIcon(hInst,MAKEINTRESOURCE(IDI_MIRANDA));
     mi.pszPopupName=(char *)-1;
     mi.position=100000;
     mi.pszName=Translate("&Move to Group");
     mi.flags=CMIF_ROOTPOPUP;
-    mi.pszContactOwner=(char *)0;
-    menuid=(HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM,wParam,(LPARAM)&mi);
-	prevmenu=menuid;
+    hMoveToGroupItem=(HANDLE)CallService(MS_CLIST_ADDCONTACTMENUITEM, 0, (LPARAM)&mi);
+  }
 
-    grpexists=TRUE;
-    i=0;
-//	intname=(char *)malloc(20);
-    grpid=1000;
-    AddGroupItem((int)menuid,Translate("Root Group"),grpid++,-1,wParam);
-    grpid+=100000;
-    //AddGroupItem(menuid,"---------------------------------------------",grpid++,0);
-    while (TRUE) {
-        _itoa(i,intname,10);
-        grpname=DBGetString(0,"CListGroups",intname);
+  {
+    int i,pos;
+    char *szGroupName,*szContactGroup;
+    char intname[20];
 
-        if (grpname==NULL ) {
-            break;
-        };
-        if (strlen(grpname)==0) {
+    if (!cbGroupsItems)
+    {
+      cbGroupsItems = 0x10;
+      hGroupsItems = (HANDLE*)malloc(cbGroupsItems*sizeof(HANDLE));
+    }
 
-            break;
-        };
-        if (grpname[0]==0) {
+    szContactGroup = DBGetString((HANDLE)wParam, "CList", "Group");
 
-            break;
-        };
-        AddGroupItem((int)menuid,&(grpname[1]),grpid++,i+1,wParam);
-    /*
-    mi.cbSize=sizeof(mi);
-    mi.hIcon=NULL;//LoadIcon(hInst,MAKEINTRESOURCE(IDI_MIRANDA));
-    mi.pszPopupName=(char *)menuid;
-    mi.popupPosition=i+1;
-    mi.position=grpid++;
-    mi.pszName=&(grpname[1]);
-    mi.flags=CMIF_CHILDPOPUP;
-    mi.pszContactOwner=(char *)0;
-    mi.pszService=MTG_MOVE;
-    CallService(MS_CLIST_ADDCONTACTMENUITEM,wParam,(LPARAM)&mi);
-    */
-        i++;
-        mir_free(grpname);
-    };
-    return 0;
-};
+    i=1;
+    pos = 1000;
+    if (!nGroupsItems)
+    {
+      nGroupsItems++;
+      hGroupsItems[0] = AddGroupItem(hMoveToGroupItem,Translate("Root Group"),pos++, -1, !szContactGroup);
+    }
+    else
+      ModifyGroupItem(hGroupsItems[0],Translate("Root Group"), !szContactGroup);
+
+    pos += 100000; // Separator
+
+
+    while (TRUE)
+    {
+      int checked;
+
+      _itoa(i-1,intname,10);
+      szGroupName = DBGetString(0,"CListGroups",intname);
+
+      if (!szGroupName || !strlen(szGroupName) || !szGroupName[0]) break;
+
+      checked = 0;
+      if (szContactGroup && !strcmp(szContactGroup, szGroupName + 1))
+        checked = 1;
+
+      if (nGroupsItems > i)
+        ModifyGroupItem(hGroupsItems[i], szGroupName + 1, checked);
+      else
+      {
+        nGroupsItems++;
+        if (cbGroupsItems < nGroupsItems)
+        {
+          cbGroupsItems += 0x10;
+          hGroupsItems = (HANDLE*)realloc(hGroupsItems, cbGroupsItems*sizeof(HANDLE));
+        }
+        hGroupsItems[i] = AddGroupItem(hMoveToGroupItem,szGroupName + 1,pos++,i,checked);
+      }
+      i++;
+      mir_free(szGroupName);
+    }
+    mir_free(szContactGroup);
+
+    while (nGroupsItems > i)
+    {
+      nGroupsItems--;
+      CallService(MS_CLIST_REMOVECONTACTMENUITEM, (WPARAM)hGroupsItems[nGroupsItems], 0);
+      hGroupsItems[nGroupsItems] = NULL;
+    }
+  }
+  return 0;
+}
+
 
 static int MTG_DOMOVE(WPARAM wParam,LPARAM lParam)
 {
-    char *grpname,*correctgrpname;
-    char *intname;
+  char *grpname;
+  char *intname;
 
 
-    lParam--;
+  lParam--;
 
-    if (lParam == -2) {//root level
-        DBDeleteContactSetting((HANDLE)wParam, "CList", "Group");
-        //DBWriteContactSettingString((HANDLE)wParam,"CList","Group","");
-        return 0;
-    }
-    intname=(char *)malloc(20);
-    _itoa(lParam,intname,10);
-    grpname=DBGetString(0,"CListGroups",intname);
-    if (grpname!=0) {
-        correctgrpname=&(grpname[1]);
-        DBWriteContactSettingString((HANDLE)wParam,"CList","Group",correctgrpname);
-        mir_free(grpname);
-    };
+  if (lParam == -2) 
+  { //root level
+    DBDeleteContactSetting((HANDLE)wParam, "CList", "Group");
 
-
-    free (intname);
     return 0;
+  }
+
+  intname=(char *)_alloca(20);
+  _itoa(lParam,intname,10);
+  grpname=DBGetString(0,"CListGroups",intname);
+  if (grpname)
+  {
+    DBWriteContactSettingString((HANDLE)wParam,"CList","Group",grpname + 1);
+    mir_free(grpname);
+  };
+
+  return 0;
 }
 
 int MTG_OnmodulesLoad(WPARAM wParam,LPARAM lParam)
 {
-    if (!ServiceExists(MS_CLIST_REMOVECONTACTMENUITEM)) {
-        MessageBoxA(0,"New menu system not found - plugin disabled.","MoveToGroup",0);
-        return(0);
-    };
-    hOnCntMenuBuild=HookEvent(ME_CLIST_PREBUILDCONTACTMENU,OnContactMenuBuild); 
-    CreateServiceFunction(MTG_MOVE,MTG_DOMOVE);
-    return(0);
-};
-
-
-/*
-int LoadMoveToGroup()
-{
-
-    hModulesLoaded=HookEvent(ME_SYSTEM_MODULESLOADED,OnmodulesLoad);	
-    
+  if (!ServiceExists(MS_CLIST_REMOVECONTACTMENUITEM)) 
+  {
+    MessageBoxA(0,"New menu system not found - plugin disabled.","MoveToGroup",0);
     return 0;
-}*/
+  }
+  hOnCntMenuBuild=HookEvent(ME_CLIST_PREBUILDCONTACTMENU,OnContactMenuBuild); 
+
+  CreateServiceFunction(MTG_MOVE,MTG_DOMOVE);
+
+  return 0;
+}
+
 
 int UnloadMoveToGroup(void)
 {
-    //if (hFrameTopWindow!=NULL){CallService(MS_CLIST_FRAMES_REMOVEFRAME,(WPARAM)hFrameTopWindow,(LPARAM)0);};
+  if (hOnCntMenuBuild)
+    UnhookEvent(hOnCntMenuBuild);
 
-    //DestroyServiceFunction();
-    UnhookEvent(hModulesLoaded);
+  nGroupsItems = 0;
+  cbGroupsItems = 0;
+  free(hGroupsItems);
 
-    return 0;
-}   
+  return 0;
+}
