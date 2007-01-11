40faf05ebb2198ebc3953720fd0c6435afa0416c
Piotr Piastucki <leech.miranda@gmail.com>
2007-01-11 00:20:23 +0000

    some visual glitches fixed
    git-svn-id: http://miranda.googlecode.com/svn/trunk@4455 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/Scriver/srmm.c
+++ miranda/plugins/Scriver/srmm.c
@@ -40,7 +40,7 @@ PLUGININFO pluginInfo = {
 #else
 	"Scriver",
 #endif
-	PLUGIN_MAKE_VERSION(2, 5, 9, 13),
+	PLUGIN_MAKE_VERSION(2, 5, 9, 14),
 #ifdef _UNICODE
 	"Scriver - send and receive instant messages (Unicode)",
 #else
