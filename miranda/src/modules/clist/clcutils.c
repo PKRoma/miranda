29419bc247ef5948de1ae7e00a25797ad282fe82
Robert Rainwater <rainwater@miranda-im.org>
2007-01-19 13:45:19 +0000

    Rollback fnHitTest clist change (fixes pin to desktop)
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@4635 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/clist/clcutils.c
+++ miranda/src/modules/clist/clcutils.c
@@ -74,17 +74,10 @@ int fnHitTest(HWND hwnd, struct ClcData *dat, int testx, int testy, struct ClcCo
 	RECT clRect;
 	HFONT hFont;
 	DWORD style = GetWindowLong(hwnd, GWL_STYLE);
-	POINT pt;
 
 	if ( flags )
 		*flags = 0;
 	
-	pt.x = testx;
-	pt.y = testy;
-	MapWindowPoints(hwnd, NULL, &pt, 1);
-	if ( ChildWindowFromPointEx( GetDesktopWindow(), pt, CWP_SKIPINVISIBLE | CWP_SKIPTRANSPARENT ) != cli.hwndContactList )
-		return -1;
-
 	GetClientRect(hwnd, &clRect);
 	if ( testx < 0 || testy < 0 || testy >= clRect.bottom || testx >= clRect.right ) {
 		if ( flags ) {
