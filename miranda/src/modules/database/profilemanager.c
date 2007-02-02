0da3a184e8d9064445d5ba756c95675d1112db0f
Robert Rainwater <rainwater@miranda-im.org>
2007-02-02 03:47:04 +0000

    Stable branch doesn't support ImageList_AddIcon_NotShared method added in last commit
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@4798 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/database/profilemanager.c
+++ miranda/src/modules/database/profilemanager.c
@@ -366,9 +366,8 @@ static BOOL CALLBACK DlgProfileSelect(HWND hwndDlg, UINT msg, WPARAM wParam, LPA
 
 			// icons
 			hImgList=ImageList_Create(16, 16, ILC_MASK | (IsWinVerXPPlus() ? ILC_COLOR32 : ILC_COLOR16), 1, 1);
-
-			ImageList_AddIcon_NotShared(hImgList, GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_USERDETAILS));
-			ImageList_AddIcon_NotShared(hImgList, GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_DELETE));
+            ImageList_AddIcon(hImgList, LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_USERDETAILS)));
+			ImageList_AddIcon(hImgList, LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_DELETE)));
 
 			// LV will destroy the image list
 			ListView_SetImageList(hwndList, hImgList, LVSIL_SMALL);
