104bbe89d003ef54b8dcd1657e5386e928cfc5e6
Artem Shpynov <ashpynov@users.sourceforge.net>
2007-03-19 13:14:59 +0000

    git-svn-id: http://miranda.googlecode.com/svn/trunk@5119 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/modernb/ExtFrames/modern_ext_frames_services.c
+++ miranda/plugins/modernb/ExtFrames/modern_ext_frames_services.c
@@ -40,4 +40,28 @@ This file have to be excluded from compilation and need to be adde to project vi
 
 #ifdef __modern_ext_frames_c__include_c_file   //protection from adding to compilation
 
+/*
+TODO services and events to be implemented:
+
+#define MS_CLIST_FRAMES_ADDFRAME			"CListFrames/AddFrame"
+#define MS_CLIST_FRAMES_REMOVEFRAME			"CListFrames/RemoveFrame"
+#define MS_CLIST_FRAMES_SHOWALLFRAMES		"CListFrames/ShowALLFrames"
+#define MS_CLIST_FRAMES_SHOWALLFRAMESTB		"CListFrames/ShowALLFramesTB"
+#define MS_CLIST_FRAMES_HIDEALLFRAMESTB		"CListFrames/HideALLFramesTB"
+#define MS_CLIST_FRAMES_SHFRAME				"CListFrames/SHFrame"
+#define MS_CLIST_FRAMES_SHFRAMETITLEBAR		"CListFrame/SHFrameTitleBar"
+#define MS_CLIST_FRAMES_ULFRAME				"CListFrame/ULFrame"
+#define MS_CLIST_FRAMES_UCOLLFRAME			"CListFrame/UCOLLFrame"
+#define MS_CLIST_FRAMES_SETUNBORDER			"CListFrame/SetUnBorder"
+#define MS_CLIST_FRAMES_UPDATEFRAME			"CListFrame/UpdateFrame"
+#define MS_CLIST_FRAMES_GETFRAMEOPTIONS		"CListFrame/GetFrameOptions"
+#define MS_CLIST_FRAMES_SETFRAMEOPTIONS		"CListFrame/SetFrameOptions"
+#define MS_CLIST_FRAMES_MENUNOTIFY			"CList/ContextFrameMenuNotify"
+
+and Events
+
+#define ME_CLIST_FRAMES_SB_SHOW_TOOLTIP		"CListFrames/StatusBarShowToolTip"
+#define ME_CLIST_FRAMES_SB_HIDE_TOOLTIP		"CListFrames/StatusBarHideToolTip"
+
+*/
 #endif
