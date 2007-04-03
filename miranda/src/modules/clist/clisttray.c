cba082fabfdbcf7b9b39dcb98d0ad429afb56c31
Artem Shpynov <ashpynov@users.sourceforge.net>
2007-04-03 08:14:33 +0000

    Fixed: Single tray icon mode and Show primary icon and only when statuses differ
    git-svn-id: http://miranda.googlecode.com/svn/trunk@5169 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/clist/clisttray.c
+++ miranda/src/modules/clist/clisttray.c
@@ -282,13 +282,15 @@ int fnTrayIconInit(HWND hwnd)
 				if ( protos[j]->type == PROTOTYPE_PROTOCOL && cli.pfnGetProtocolVisibility( protos[j]->szName ))
 					cli.pfnTrayIconAdd(hwnd, protos[j]->szName, NULL, CallProtoService(protos[j]->szName, PS_GETSTATUS, 0, 0));
 	}	}
-	else if (averageMode <= 0 && DBGetContactSettingByte(NULL, "CList", "cli.trayIcon", SETTING_TRAYICON_DEFAULT) == SETTING_TRAYICON_SINGLE) {
+	else if (averageMode <= ID_STATUS_OFFLINE && DBGetContactSettingByte(NULL, "CList", "TrayIcon", SETTING_TRAYICON_DEFAULT) == SETTING_TRAYICON_SINGLE) {
 		DBVARIANT dbv = { DBVT_DELETED };
 		char *szProto;
-		if (DBGetContactSetting(NULL, "CList", "PrimaryStatus", &dbv))
-			szProto = NULL;
+		if (!DBGetContactSetting(NULL, "CList", "PrimaryStatus", &dbv) 
+			 && (averageMode <= 0 || DBGetContactSettingByte(NULL, "CList", "AlwaysPrimary", 0) ))
+			szProto = dbv.pszVal;	
 		else
-			szProto = dbv.pszVal;
+			szProto = NULL;
+
 		cli.pfnTrayIconAdd(hwnd, NULL, szProto, szProto ? CallProtoService(szProto, PS_GETSTATUS, 0, 0) : CallService(MS_CLIST_GETSTATUSMODE, 0, 0));
 		DBFreeVariant(&dbv);
 	}
