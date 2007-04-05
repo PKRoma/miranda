9ab073e5b1fbaa91f5ddd4ed50f4b90056ff21ee
Robert Rainwater <rainwater@miranda-im.org>
2007-04-05 21:58:32 +0000

    Fix ASSERT warning on some compilers with extracticon.c.  Also clean up header to use commonheaders.h. (Patch by OnO)
    git-svn-id: http://miranda.googlecode.com/svn/trunk@5185 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/icolib/extracticon.c
+++ miranda/src/modules/icolib/extracticon.c
@@ -20,8 +20,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
-#include <windows.h>
+#include "../../core/commonheaders.h"
 
+#ifdef _ASSERT
+#undef _ASSERT
+#endif
 #define _ASSERT(n)
 // http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/winui/windowsuserinterface/resources/introductiontoresources/resourcereference/resourcestructures/newheader.asp
 typedef struct
