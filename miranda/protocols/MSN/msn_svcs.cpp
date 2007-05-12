79deb9c6aea22f8981898d7fded82e8325dbfaf7
Boris Krasnovskiy <borkra@users.sourceforge.net>
2007-05-12 08:07:11 +0000

    Fixed deleting avatar
    git-svn-id: http://miranda.googlecode.com/svn/trunk@5424 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_svcs.cpp
+++ miranda/protocols/MSN/msn_svcs.cpp
@@ -1024,77 +1024,88 @@ static int MsnSetApparentMode( WPARAM wParam, LPARAM lParam )
 static int MsnSetAvatar( WPARAM wParam, LPARAM lParam )
 {
 	char* szFileName = ( char* )lParam;
-	int fileId = _open( szFileName, _O_RDONLY | _O_BINARY, _S_IREAD );
-	if ( fileId == -1 )
-		return 1;
 
-	long  dwPngSize = filelength( fileId );
-	BYTE* pResult = new BYTE[ dwPngSize ];
-	if ( pResult == NULL )
-		return 2;
-
-	_read( fileId, pResult, dwPngSize );
-	_close( fileId );
-
-	mir_sha1_ctx sha1ctx;
-	BYTE sha1c[ MIR_SHA1_HASH_SIZE ], sha1d[ MIR_SHA1_HASH_SIZE ];
-	char szSha1c[ 40 ], szSha1d[ 40 ];
-	mir_sha1_init( &sha1ctx );
-	mir_sha1_append( &sha1ctx, pResult, dwPngSize );
-	mir_sha1_finish( &sha1ctx, sha1d );
-	{	NETLIBBASE64 nlb = { szSha1d, sizeof( szSha1d ), ( PBYTE )sha1d, sizeof( sha1d ) };
-		MSN_CallService( MS_NETLIB_BASE64ENCODE, 0, LPARAM( &nlb ));
-	}
-	char drive[_MAX_DRIVE];
-	char dir[_MAX_DIR];
-	char fname[_MAX_FNAME];
-	char ext[_MAX_EXT];
-	_splitpath( szFileName, drive, dir, fname, ext );
-	mir_sha1_init( &sha1ctx );
-
-	mir_sha1_append( &sha1ctx, ( PBYTE )"Creator", 7 );
-	mir_sha1_append( &sha1ctx, ( PBYTE )MyOptions.szEmail, strlen( MyOptions.szEmail ));
-
-	char szFileSize[ 20 ];
-	ltoa( dwPngSize, szFileSize, 10 );
-	mir_sha1_append( &sha1ctx, ( PBYTE )"Size", 4 );
-	mir_sha1_append( &sha1ctx, ( PBYTE )szFileSize, strlen( szFileSize ));
-
-	mir_sha1_append( &sha1ctx, ( PBYTE )"Type", 4 );
-	mir_sha1_append( &sha1ctx, ( PBYTE )"3", 1 );  // MSN_TYPEID_DISPLAYPICT
-
-	mir_sha1_append( &sha1ctx, ( PBYTE )"Location", 8 );
-	mir_sha1_append( &sha1ctx, ( PBYTE )fname, sizeof( fname ));
-
-	mir_sha1_append( &sha1ctx, ( PBYTE )"Friendly", 8 );
-	mir_sha1_append( &sha1ctx, ( PBYTE )"AAA=", 4 );
-
-	mir_sha1_append( &sha1ctx, ( PBYTE )"SHA1D", 5 );
-	mir_sha1_append( &sha1ctx, ( PBYTE )szSha1d, strlen( szSha1d ));
-	mir_sha1_finish( &sha1ctx, sha1c );
-	{	NETLIBBASE64 nlb = { szSha1c, sizeof( szSha1c ), ( PBYTE )sha1c, sizeof( sha1c ) };
-		MSN_CallService( MS_NETLIB_BASE64ENCODE, 0, LPARAM( &nlb ));
-	}
+	if (szFileName == NULL)
 	{
-		char* szBuffer = ( char* )alloca( 1000 );
-		mir_snprintf( szBuffer, 1000,
-			"<msnobj Creator=\"%s\" Size=\"%ld\" Type=\"3\" Location=\"%s\" Friendly=\"AAA=\" SHA1D=\"%s\" SHA1C=\"%s\"/>",
-			MyOptions.szEmail, dwPngSize,fname, szSha1d, szSha1c );
-
-		char* szEncodedBuffer = ( char* )alloca( 1000 );
-		UrlEncode( szBuffer, szEncodedBuffer, 1000 );
-
-		MSN_SetString( NULL, "PictObject", szEncodedBuffer );
-	}
-	{	char tFileName[ MAX_PATH ];
+		char tFileName[ MAX_PATH ];
 		MSN_GetAvatarFileName( NULL, tFileName, sizeof( tFileName ));
-		int fileId = _open( tFileName, _O_CREAT | _O_TRUNC | _O_WRONLY | O_BINARY,  _S_IREAD | _S_IWRITE );
-		if ( fileId > -1 ) {
-			_write( fileId, pResult, dwPngSize );
-			_close( fileId );
-	}	}
+		remove( tFileName );
+		MSN_DeleteSetting( NULL, "PictObject" );
+	}
+	else
+	{
+		int fileId = _open( szFileName, _O_RDONLY | _O_BINARY, _S_IREAD );
+		if ( fileId == -1 )
+			return 1;
 
-	delete pResult;
+		long  dwPngSize = filelength( fileId );
+		BYTE* pResult = new BYTE[ dwPngSize ];
+		if ( pResult == NULL )
+			return 2;
+
+		_read( fileId, pResult, dwPngSize );
+		_close( fileId );
+
+		mir_sha1_ctx sha1ctx;
+		BYTE sha1c[ MIR_SHA1_HASH_SIZE ], sha1d[ MIR_SHA1_HASH_SIZE ];
+		char szSha1c[ 40 ], szSha1d[ 40 ];
+		mir_sha1_init( &sha1ctx );
+		mir_sha1_append( &sha1ctx, pResult, dwPngSize );
+		mir_sha1_finish( &sha1ctx, sha1d );
+		{	NETLIBBASE64 nlb = { szSha1d, sizeof( szSha1d ), ( PBYTE )sha1d, sizeof( sha1d ) };
+			MSN_CallService( MS_NETLIB_BASE64ENCODE, 0, LPARAM( &nlb ));
+		}
+		char drive[_MAX_DRIVE];
+		char dir[_MAX_DIR];
+		char fname[_MAX_FNAME];
+		char ext[_MAX_EXT];
+		_splitpath( szFileName, drive, dir, fname, ext );
+		mir_sha1_init( &sha1ctx );
+
+		mir_sha1_append( &sha1ctx, ( PBYTE )"Creator", 7 );
+		mir_sha1_append( &sha1ctx, ( PBYTE )MyOptions.szEmail, strlen( MyOptions.szEmail ));
+
+		char szFileSize[ 20 ];
+		ltoa( dwPngSize, szFileSize, 10 );
+		mir_sha1_append( &sha1ctx, ( PBYTE )"Size", 4 );
+		mir_sha1_append( &sha1ctx, ( PBYTE )szFileSize, strlen( szFileSize ));
+
+		mir_sha1_append( &sha1ctx, ( PBYTE )"Type", 4 );
+		mir_sha1_append( &sha1ctx, ( PBYTE )"3", 1 );  // MSN_TYPEID_DISPLAYPICT
+
+		mir_sha1_append( &sha1ctx, ( PBYTE )"Location", 8 );
+		mir_sha1_append( &sha1ctx, ( PBYTE )fname, sizeof( fname ));
+
+		mir_sha1_append( &sha1ctx, ( PBYTE )"Friendly", 8 );
+		mir_sha1_append( &sha1ctx, ( PBYTE )"AAA=", 4 );
+
+		mir_sha1_append( &sha1ctx, ( PBYTE )"SHA1D", 5 );
+		mir_sha1_append( &sha1ctx, ( PBYTE )szSha1d, strlen( szSha1d ));
+		mir_sha1_finish( &sha1ctx, sha1c );
+		{	NETLIBBASE64 nlb = { szSha1c, sizeof( szSha1c ), ( PBYTE )sha1c, sizeof( sha1c ) };
+			MSN_CallService( MS_NETLIB_BASE64ENCODE, 0, LPARAM( &nlb ));
+		}
+		{
+			char szBuffer[ 1000 ];
+			mir_snprintf( szBuffer, sizeof( szBuffer ),
+				"<msnobj Creator=\"%s\" Size=\"%ld\" Type=\"3\" Location=\"%s\" Friendly=\"AAA=\" SHA1D=\"%s\" SHA1C=\"%s\"/>",
+				MyOptions.szEmail, dwPngSize, fname, szSha1d, szSha1c );
+
+			char szEncodedBuffer = [ 2000 ];
+			UrlEncode( szBuffer, szEncodedBuffer, sizeof( szEncodedBuffer ));
+
+			MSN_SetString( NULL, "PictObject", szEncodedBuffer );
+		}
+		{	char tFileName[ MAX_PATH ];
+			MSN_GetAvatarFileName( NULL, tFileName, sizeof( tFileName ));
+			int fileId = _open( tFileName, _O_CREAT | _O_TRUNC | _O_WRONLY | O_BINARY,  _S_IREAD | _S_IWRITE );
+			if ( fileId > -1 ) {
+				_write( fileId, pResult, dwPngSize );
+				_close( fileId );
+		}	}
+
+		delete pResult;
+	}
 
 	MSN_SetServerStatus( msnStatusMode );
 	return 0;
