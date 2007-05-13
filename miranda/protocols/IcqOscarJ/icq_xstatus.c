0107b232251ae8f2f33827ac81985243edf26dcb
Josef Kučera <jokusoftware@users.sourceforge.net>
2007-05-13 08:48:12 +0000

    update file header
    git-svn-id: http://miranda.googlecode.com/svn/trunk@5430 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/IcqOscarJ/icq_xstatus.c
+++ miranda/protocols/IcqOscarJ/icq_xstatus.c
@@ -23,7 +23,7 @@
 //
 // -----------------------------------------------------------------------------
 //
-// File name      : $Source: /cvsroot/miranda/miranda/protocols/IcqOscarJ/icq_xstatus.c,v $
+// File name      : $URL$
 // Revision       : $Revision$
 // Last change on : $Date$
 // Last change by : $Author$
