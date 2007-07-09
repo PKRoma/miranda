d80cb8ad058d773c761c01f08cbe13325c097c0a
Adam Strzelecki <ono@users.sourceforge.net>
2007-07-09 16:53:31 +0000

    * Added Unicode<->ANSI conversion inline functions * Formatting fixes
    git-svn-id: http://miranda.googlecode.com/svn/trunk@5848 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/include/m_utils.h
+++ miranda/include/m_utils.h
@@ -40,7 +40,7 @@ bOpenInNewWindow should be zero to open the URL in the browser window the user
 last used, or nonzero to open in a new browser window. If there's no browser
 running, one will be opened to show the URL.
 */
-#define MS_UTILS_OPENURL    "Utils/OpenURL"
+#define MS_UTILS_OPENURL	"Utils/OpenURL"
 
 /* Resizes a dialog by calling a custom routine to move the individual
 controls   v0.1.0.1+
@@ -58,7 +58,7 @@ typedef struct {
 	int cbSize;
 	UINT wId;				//control ID
 	RECT rcItem;			//original control rectangle, relative to dialog
-                            //modify in-place to specify the new position
+							//modify in-place to specify the new position
 	SIZE dlgOriginalSize;	//size of dialog client area in template
 	SIZE dlgNewSize;		//current size of dialog client area
 } UTILRESIZECONTROL;
@@ -71,19 +71,19 @@ typedef struct {
 	LPARAM lParam;			//caller-defined
 	DIALOGRESIZERPROC pfnResizer;
 } UTILRESIZEDIALOG;
-#define RD_ANCHORX_CUSTOM   0	//function did everything required to the x axis, do no more processing
-#define RD_ANCHORX_LEFT     0	//move the control to keep it constant distance from the left edge of the dialog
-#define RD_ANCHORX_RIGHT    1	//move the control to keep it constant distance from the right edge of the dialog
-#define RD_ANCHORX_WIDTH    2	//size the control to keep it constant distance from both edges of the dialog
-#define RD_ANCHORX_CENTRE   4	//move the control to keep it constant distance from the centre of the dialog
-#define RD_ANCHORY_CUSTOM   0
-#define RD_ANCHORY_TOP      0
-#define RD_ANCHORY_BOTTOM   8
-#define RD_ANCHORY_HEIGHT   16
-#define RD_ANCHORY_CENTRE   32
+#define RD_ANCHORX_CUSTOM	0	//function did everything required to the x axis, do no more processing
+#define RD_ANCHORX_LEFT 	0	//move the control to keep it constant distance from the left edge of the dialog
+#define RD_ANCHORX_RIGHT	1	//move the control to keep it constant distance from the right edge of the dialog
+#define RD_ANCHORX_WIDTH	2	//size the control to keep it constant distance from both edges of the dialog
+#define RD_ANCHORX_CENTRE	4	//move the control to keep it constant distance from the centre of the dialog
+#define RD_ANCHORY_CUSTOM	0
+#define RD_ANCHORY_TOP		0
+#define RD_ANCHORY_BOTTOM	8
+#define RD_ANCHORY_HEIGHT	16
+#define RD_ANCHORY_CENTRE	32
 #define MS_UTILS_RESIZEDIALOG	 "Utils/ResizeDialog"
 
-/* Gets the name of a country given its number      v0.1.2.0+
+/* Gets the name of a country given its number		v0.1.2.0+
 wParam=countryId
 lParam=0
 Returns a pointer to the string containing the country name on success,
@@ -91,7 +91,7 @@ or NULL on failure
 */
 #define MS_UTILS_GETCOUNTRYBYNUMBER   "Utils/GetCountryByNumber"
 
-/* Gets the full list of country IDs     v0.1.2.0+
+/* Gets the full list of country IDs	 v0.1.2.0+
 wParam=(WPARAM)(int*)piCount
 lParam=(LPARAM)(struct CountryListEntry**)ppList
 Returns 0 always
@@ -179,7 +179,7 @@ __inline static int WindowList_BroadcastAsync(HANDLE hList,UINT message,WPARAM w
 /***************************** Hyperlink windows ********************************/
 
 //there aren't any services here, because you don't need them.
-#define WNDCLASS_HYPERLINK   _T("Hyperlink")
+#define WNDCLASS_HYPERLINK	 _T("Hyperlink")
 //the control will obey the SS_LEFT (0), SS_CENTER (1), and SS_RIGHT (2) styles
 //the control will send STN_CLICKED via WM_COMMAND when the link itself is clicked
 
@@ -219,10 +219,10 @@ __inline static int Utils_SaveWindowPosition(HWND hwnd,HANDLE hContact,const cha
 //nothing
 //the NoSize version won't use stored size information: the window is left the
 //same size.
-#define RWPF_NOSIZE     1  //don't use stored size info: leave dialog same size
-#define RWPF_NOMOVE     2  //don't use stored position
+#define RWPF_NOSIZE 	1  //don't use stored size info: leave dialog same size
+#define RWPF_NOMOVE 	2  //don't use stored position
 #define RWPF_NOACTIVATE 4  //show but don't activate v0.3.3.0+
-#define MS_UTILS_RESTOREWINDOWPOSITION  "Utils/RestoreWindowPos"
+#define MS_UTILS_RESTOREWINDOWPOSITION	"Utils/RestoreWindowPos"
 __inline static int Utils_RestoreWindowPosition(HWND hwnd,HANDLE hContact,const char *szModule,const char *szNamePrefix) {
 	SAVEWINDOWPOS swp;
 	swp.hwnd=hwnd; swp.hContact=hContact; swp.szModule=szModule; swp.szNamePrefix=szNamePrefix;
@@ -243,11 +243,11 @@ __inline static int Utils_RestoreWindowPositionNoMove(HWND hwnd,HANDLE hContact,
 
 #define WNDCLASS_COLOURPICKER  _T("ColourPicker")
 
-#define CPM_SETCOLOUR          0x1000	  //lParam=new colour
-#define CPM_GETCOLOUR          0x1001	  //returns colour
+#define CPM_SETCOLOUR		   0x1000	  //lParam=new colour
+#define CPM_GETCOLOUR		   0x1001	  //returns colour
 #define CPM_SETDEFAULTCOLOUR   0x1002	  //lParam=default, used as first custom colour
 #define CPM_GETDEFAULTCOLOUR   0x1003	  //returns colour
-#define CPN_COLOURCHANGED      1		  //sent through WM_COMMAND
+#define CPN_COLOURCHANGED	   1		  //sent through WM_COMMAND
 
 /***************************** Bitmap Filter (0.1.2.1+) *************************/
 
@@ -262,7 +262,7 @@ __inline static int Utils_RestoreWindowPositionNoMove(HWND hwnd,HANDLE hContact,
 //Remember to DeleteObject() when you're done
 #define MS_UTILS_LOADBITMAP   "Utils/LoadBitmap"
 
-//Gets the filter strings for use in the open file dialog      v0.1.2.1+
+//Gets the filter strings for use in the open file dialog	   v0.1.2.1+
 //wParam=cbLengthOfBuffer
 //lParam=(LPARAM)(char*)pszBuffer
 //Returns 0 on success, nonzero on failure
@@ -311,21 +311,21 @@ typedef unsigned char mir_md5_byte_t; /* 8-bit byte */
 typedef unsigned int mir_md5_word_t; /* 32-bit word */
 
 typedef struct mir_md5_state_s {
-    mir_md5_word_t count[2];  /* message length in bits, lsw first */
-    mir_md5_word_t abcd[4];    /* digest buffer */
-    mir_md5_byte_t buf[64];    /* accumulate block */
+	mir_md5_word_t count[2];  /* message length in bits, lsw first */
+	mir_md5_word_t abcd[4];    /* digest buffer */
+	mir_md5_byte_t buf[64];    /* accumulate block */
 } mir_md5_state_t;
 
 struct MD5_INTERFACE
 {
 	int cbSize;
-    void (*md5_init) (mir_md5_state_t *pms);
-    void (*md5_append) (mir_md5_state_t *pms, const mir_md5_byte_t *data, int nbytes);
-    void (*md5_finish) (mir_md5_state_t *pms, mir_md5_byte_t digest[16]);
-    void (*md5_hash) (const mir_md5_byte_t *data, int len, mir_md5_byte_t digest[16]);
+	void (*md5_init) (mir_md5_state_t *pms);
+	void (*md5_append) (mir_md5_state_t *pms, const mir_md5_byte_t *data, int nbytes);
+	void (*md5_finish) (mir_md5_state_t *pms, mir_md5_byte_t digest[16]);
+	void (*md5_hash) (const mir_md5_byte_t *data, int len, mir_md5_byte_t digest[16]);
 };
 
-#define MS_SYSTEM_GET_MD5I  "Miranda/System/GetMD5I"
+#define MS_SYSTEM_GET_MD5I	"Miranda/System/GetMD5I"
 
 static __inline int mir_getMD5I( struct MD5_INTERFACE* dest )
 {
@@ -335,10 +335,10 @@ static __inline int mir_getMD5I( struct MD5_INTERFACE* dest )
 
 extern struct MD5_INTERFACE md5i;
 
-#define mir_md5_init(A)         md5i.md5_init(A)
-#define mir_md5_append(A,B,C)   md5i.md5_append(A,B,C)
-#define mir_md5_finish(A,B)     md5i.md5_finish(A,B)
-#define mir_md5_hash(A,B,C)     md5i.md5_hash(A,B,C)
+#define mir_md5_init(A) 		md5i.md5_init(A)
+#define mir_md5_append(A,B,C)	md5i.md5_append(A,B,C)
+#define mir_md5_finish(A,B) 	md5i.md5_finish(A,B)
+#define mir_md5_hash(A,B,C) 	md5i.md5_hash(A,B,C)
 
 /*
 	SHA1 interface. 0.7.0.12
@@ -359,10 +359,10 @@ typedef struct {
 struct SHA1_INTERFACE
 {
 	int cbSize;
-    void (*sha1_init) (mir_sha1_ctx *ctx);
-    void (*sha1_append) (mir_sha1_ctx *ctx, mir_sha1_byte_t *dataIn, int len);
-    void (*sha1_finish) (mir_sha1_ctx *ctx, mir_sha1_byte_t hashout[20]);
-    void (*sha1_hash) (mir_sha1_byte_t *dataIn, int len, mir_sha1_byte_t hashout[20]);
+	void (*sha1_init) (mir_sha1_ctx *ctx);
+	void (*sha1_append) (mir_sha1_ctx *ctx, mir_sha1_byte_t *dataIn, int len);
+	void (*sha1_finish) (mir_sha1_ctx *ctx, mir_sha1_byte_t hashout[20]);
+	void (*sha1_hash) (mir_sha1_byte_t *dataIn, int len, mir_sha1_byte_t hashout[20]);
 };
 
 #define MS_SYSTEM_GET_SHA1I  "Miranda/System/GetSHA1I"
@@ -375,10 +375,10 @@ static __inline int mir_getSHA1I( struct SHA1_INTERFACE* dest )
 
 extern struct SHA1_INTERFACE sha1i;
 
-#define mir_sha1_init(A)         sha1i.sha1_init(A)
-#define mir_sha1_append(A,B,C)   sha1i.sha1_append(A,B,C)
-#define mir_sha1_finish(A,B)     sha1i.sha1_finish(A,B)
-#define mir_sha1_hash(A,B,C)     sha1i.sha1_hash(A,B,C)
+#define mir_sha1_init(A)		 sha1i.sha1_init(A)
+#define mir_sha1_append(A,B,C)	 sha1i.sha1_append(A,B,C)
+#define mir_sha1_finish(A,B)	 sha1i.sha1_finish(A,B)
+#define mir_sha1_hash(A,B,C)	 sha1i.sha1_hash(A,B,C)
 
 // Added in 0.4.0.1
 // Here are some string wrappers that are more safe than the win32 versions
@@ -414,11 +414,11 @@ static __inline int mir_vsnprintf(char *buffer, size_t count, const char* fmt, v
 }
 
 static __inline int mir_vsntprintf(TCHAR *buffer, size_t count, const TCHAR* fmt, va_list va) {
-    int len;
+	int len;
 
-    len = _vsntprintf(buffer, count-1, fmt, va);
-    buffer[count-1] = 0;
-    return len;
+	len = _vsntprintf(buffer, count-1, fmt, va);
+	buffer[count-1] = 0;
+	return len;
 }
 
 // allows to include TCHAR* strings into mir_snprintf and NetLib_Logf calls
@@ -426,6 +426,46 @@ static __inline int mir_vsntprintf(TCHAR *buffer, size_t count, const TCHAR* fmt
 	#define TCHAR_STR_PARAM "%S"
 #else
 	#define TCHAR_STR_PARAM "%s"
+#endif
+
+static __inline wchar_t* mir_a2u( char* src )
+{
+	int codepage = CallService("LangPack/GetCodePage", 0, 0 );
+
+	int cbLen = MultiByteToWideChar( codepage, 0, src, -1, NULL, 0 );
+	wchar_t* result = ( wchar_t* )malloc( sizeof( wchar_t )*(cbLen+1));
+	if ( result == NULL )
+		return NULL;
+
+	MultiByteToWideChar( codepage, 0, src, -1, result, cbLen );
+	result[ cbLen ] = 0;
+	return result;
+}
+
+static __inline char* mir_u2a( wchar_t* src )
+{
+	int codepage = CallService("LangPack/GetCodePage", 0, 0 );
+
+	int cbLen = WideCharToMultiByte( codepage, 0, src, -1, NULL, 0, NULL, NULL );
+	char* result = ( char* )malloc( cbLen+1 );
+	if ( result == NULL )
+		return NULL;
+
+	WideCharToMultiByte( codepage, 0, src, -1, result, cbLen, NULL, NULL );
+	result[ cbLen ] = 0;
+	return result;
+}
+
+#ifdef _UNICODE
+#define mir_t2a(s) mir_u2a(s)
+#define mir_a2t(s) mir_a2u(s)
+#define mir_t2u(s) mir_wstrdup(s)
+#define mir_u2t(s) mir_wstrdup(s)
+#else
+#define mir_t2a(s) mir_strdup(s)
+#define mir_a2t(s) mir_strdup(s)
+#define mir_t2u(s) mir_a2u(s)
+#define mir_u2t(s) mir_u2a(s)
 #endif
 
 #endif // M_UTILS_H__
