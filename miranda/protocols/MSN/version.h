93c44c256d310844f8ed3b2f0b6a0473e42e5018
Boris Krasnovskiy <borkra@users.sourceforge.net>
2007-07-18 08:04:18 +0000

    git-svn-id: http://miranda.googlecode.com/svn/trunk@5905 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/version.h
+++ miranda/protocols/MSN/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,7,1,0
-#define __VERSION_STRING            "0.7.1.0"
+#define __FILEVERSION_STRING        0,7,1,1
+#define __VERSION_STRING            "0.7.1.1"
 #define __VERSION_DWORD             0x00070100
