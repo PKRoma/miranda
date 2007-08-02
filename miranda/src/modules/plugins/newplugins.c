9ca4c346087948decf7509a4b479bb37f5612428
Robert Rainwater <rainwater@miranda-im.org>
2007-08-02 22:07:30 +0000

    Added more plugins to the block list.
    git-svn-id: http://miranda.googlecode.com/svn/trunk@6101 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/plugins/newplugins.c
+++ miranda/src/modules/plugins/newplugins.c
@@ -171,7 +171,8 @@ static int isPluginBanned(MUUID u1) {
 
 static char* modulesToSkip[] = { "autoloadavatars.dll", "multiwindow.dll", "fontservice.dll", "icolib.dll" };
 // The following plugins will be checked for a valid MUUID or they will not be loaded
-static char* expiredModulesToSkip[] = { "scriver.dll", "nconvers.dll", "tabsrmm.dll", "nhistory.dll", "historypp.dll", "help.dll", "loadavatars.dll" };
+static char* expiredModulesToSkip[] = { "scriver.dll", "nconvers.dll", "tabsrmm.dll", "nhistory.dll", "historypp.dll", "help.dll", "loadavatars.dll",
+                                        "tabsrmm_unicode.dll", "clist_nicer_plus.dll", "changeinfo.dll" };
 
 static int checkAPI(char * plugin, BASIC_PLUGIN_INFO * bpi, DWORD mirandaVersion, int checkTypeAPI, int * exports)
 {
