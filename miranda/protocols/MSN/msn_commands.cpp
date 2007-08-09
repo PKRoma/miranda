a1eefc31d12e2d2b5d1a39dfd39092b963541460
Boris Krasnovskiy <borkra@users.sourceforge.net>
2007-08-09 11:11:30 +0000

    Added automatic status message update notification
    git-svn-id: http://miranda.googlecode.com/svn/trunk@6172 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_commands.cpp
+++ miranda/protocols/MSN/msn_commands.cpp
@@ -662,6 +662,8 @@ static void sttProcessStatusMessage( char* buf, unsigned len, HANDLE hContact )
 	else
 		DBDeleteContactSetting( hContact, "CList", "StatusMsg" );
 
+	CallContactService(hContact, PSS_GETAWAYMSG, 0, 0);
+	
 	// Process current media info
 	const char* szCrntMda = ezxml_txt(ezxml_child(xmli, "CurrentMedia"));
 	if (!*szCrntMda)
