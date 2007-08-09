23a98b79b1598640bd790b1ced64c373af5cd33d
Boris Krasnovskiy <borkra@users.sourceforge.net>
2007-08-09 10:53:43 +0000

    Revert last change
    git-svn-id: http://miranda.googlecode.com/svn/trunk@6169 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_commands.cpp
+++ miranda/protocols/MSN/msn_commands.cpp
@@ -662,8 +662,6 @@ static void sttProcessStatusMessage( char* buf, unsigned len, HANDLE hContact )
 	else
 		DBDeleteContactSetting( hContact, "CList", "StatusMsg" );
 
-	CallProtoService(msnProtocolName, PSS_GETAWAYMSG, 0, 0);
-
 	// Process current media info
 	const char* szCrntMda = ezxml_txt(ezxml_child(xmli, "CurrentMedia"));
 	if (!*szCrntMda)
