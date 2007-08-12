92a48189aa2a4a537e7465ccdbb81a04dfe4d3e4
Maxim Mluhov <maxim.mluhov@gmail.com>
2007-08-12 17:57:53 +0000

    version bump before RC
    git-svn-id: http://miranda.googlecode.com/svn/trunk@6225 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/version.h
+++ miranda/protocols/JabberG/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,7,0,12
-#define __VERSION_STRING            "0.7.0.12"
-#define __VERSION_DWORD             0x0007000C
+#define __FILEVERSION_STRING        0,7,0,13
+#define __VERSION_STRING            "0.7.0.13"
+#define __VERSION_DWORD             0x0007000D
