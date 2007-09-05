d8982635fd977f831f8436dacfe7ec227d9b2efc
Robert Rainwater <rainwater@miranda-im.org>
2007-09-05 13:34:13 +0000

    DB string handling changes for Scriver
    git-svn-id: http://miranda.googlecode.com/svn/trunk@6417 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/Scriver/chat/options.c
+++ miranda/plugins/Scriver/chat/options.c
@@ -1122,7 +1122,7 @@ void LoadGlobalSettings(void)
 		char pszTemp[MAX_PATH];
 		DBVARIANT dbv;
 		g_Settings.pszLogDir = (char *)mir_realloc(g_Settings.pszLogDir, MAX_PATH);
-		if (!DBGetContactSetting(NULL, "Chat", "LogDirectory", &dbv) && dbv.type == DBVT_ASCIIZ)
+		if (!DBGetContactSettingString(NULL, "Chat", "LogDirectory", &dbv))
 		{
 			lstrcpynA(pszTemp, dbv.pszVal, MAX_PATH);
 			DBFreeVariant(&dbv);
@@ -1230,6 +1230,7 @@ int OptionsUnInit(void)
 	DeleteObject(g_Settings.NameFont);
 	return 0;
 }
+
 
 
 
