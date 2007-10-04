6f47a867f553ee9dbb2021238603369810ea9507
Maxim Mluhov <maxim.mluhov@gmail.com>
2007-10-04 12:12:05 +0000

    fixed bug #0000059: urn:xmpp:time returns month number off by 1
    git-svn-id: http://miranda.googlecode.com/svn/trunk@6523 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/jabber_iq_handlers.cpp
+++ miranda/protocols/JabberG/jabber_iq_handlers.cpp
@@ -167,8 +167,8 @@ void JabberProcessIqTime202( XmlNode* node, void* userdata, CJabberIqInfo *pInfo
 	_tzset();
 	time(&ltime);
 	gmt = gmtime(&ltime);
-	sprintf(stime,"%.4i-%.2i-%.2iT%.2i:%.2i:%.2iZ", gmt->tm_year + 1900, gmt->tm_mon, gmt->tm_mday,
-		gmt->tm_hour, gmt->tm_min, gmt->tm_sec);
+	sprintf(stime,"%.4i-%.2i-%.2iT%.2i:%.2i:%.2iZ", gmt->tm_year + 1900, gmt->tm_mon + 1,
+		gmt->tm_mday, gmt->tm_hour, gmt->tm_min, gmt->tm_sec);
 
 	int nGmtOffset = GetGMTOffset();
 	ltime = abs(nGmtOffset);
