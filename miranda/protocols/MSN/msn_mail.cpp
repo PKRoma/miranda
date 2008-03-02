a059583bf0f08b3333c5fea795af7768ebefade8
Boris Krasnovskiy <borkra@users.sourceforge.net>
2008-03-02 20:44:20 +0000

    Fixed crash
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@7379 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_mail.cpp
+++ miranda/protocols/MSN/msn_mail.cpp
@@ -121,10 +121,8 @@ void getOIMs(ezxml_t xmli)
 				}
 			}
 
-			char* szMsg = mailInfo.decodeMailBody((char*)mailbody);
-
 			PROTORECVEVENT pre = {0};
-			pre.szMessage = szMsg;
+			pre.szMessage = mailInfo.decodeMailBody((char*)mailbody);
 			pre.flags = PREF_UTF /*+ (( isRtl ) ? PREF_RTL : 0)*/;
 			pre.timestamp = evtm;
 
@@ -138,7 +136,6 @@ void getOIMs(ezxml_t xmli)
 			ezxml_t delmid = ezxml_add_child(delmids, "messageId", 0);
 			ezxml_set_txt(delmid, szId);
 			
-			mir_free( szMsg );
 			ezxml_free(tokm);
 		}
 		mir_free( tResult );
