74b619c20c86e25a2c07a9a8c9e1d0768536d040
George Hazan <ghazan@miranda-im.org>
2008-03-11 08:15:50 +0000

    version bump
    git-svn-id: http://miranda.googlecode.com/svn/trunk@7436 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/IRCG/version.h
+++ miranda/protocols/IRCG/version.h
@@ -1,6 +1,6 @@
-#define __FILEVERSION_STRING        0,8,0,3
-#define __VERSION_STRING            "0.8.0.3"
-#define __VERSION_DWORD             0x00080003
+#define __FILEVERSION_STRING        0,8,0,4
+#define __VERSION_STRING            "0.8.0.4"
+#define __VERSION_DWORD             0x00080004
 
 #define __DESC	                  "IRC protocol for Miranda IM"
 #define __AUTHOR                 "Miranda team"
