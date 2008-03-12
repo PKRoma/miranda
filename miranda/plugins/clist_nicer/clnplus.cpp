263fb941c64f7b14bce44f14cbd46497357d2cb4
George Hazan <ghazan@miranda-im.org>
2008-03-12 15:21:08 +0000

    removed useless headers from clnplus.cpp
    git-svn-id: http://miranda.googlecode.com/svn/trunk@7442 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist_nicer/clnplus.cpp
+++ miranda/plugins/clist_nicer/clnplus.cpp
@@ -4,49 +4,10 @@
 
 #include "m_stdhdr.h"
 
-#include <string>
-
 #define _CLN_GDIP
 
 #define _WIN32_WINNT 0x0501
-#include <windows.h>
-#include <commctrl.h>
-#include <stdio.h>
-#include <time.h>
-#include <stddef.h>
-#include <process.h>
 #include <shobjidl.h>
-#include <io.h>
-#include <string.h>
-#include <direct.h>
-#include <math.h>
-#include "resource.h"
-#include "forkthread.h"
-#include <win2k.h>
-#include <newpluginapi.h>
-#include <m_clist.h>
-#include <m_clistint.h>
-#include <m_clc.h>
-#include <m_clui.h>
-#include <m_plugins.h>
-#include <m_system.h>
-#include <m_database.h>
-#include <m_langpack.h>
-#include <m_button.h>
-#include <m_options.h>
-#include <m_protosvc.h>
-#include <m_utils.h>
-#include <m_skin.h>
-#include <m_contacts.h>
-#include <m_file.h>
-#include <m_addcontact.h>
-#include "m_cln_skinedit.h"
-#include "extbackg.h"
-#include "clc.h"
-#include "clist.h"
-#include "alphablend.h"
-#include "m_avatars.h"
-#include "m_popup.h"
 
 #undef Translate
 
