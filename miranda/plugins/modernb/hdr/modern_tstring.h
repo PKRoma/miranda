bbc9f7b7b7b86a9922f1e99e53583d35fc6d20bd
Artem Shpynov <ashpynov@users.sourceforge.net>
2008-08-19 15:49:51 +0000

    Added MString from IRC|Jabber as base and written Wrapper around for ANSI<->UNICODE<->UTF8 conversion (astring, wstring (tstring), mbstring)
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8255 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/modernb/hdr/modern_tstring.h
+++ miranda/plugins/modernb/hdr/modern_tstring.h
@@ -1,52 +1,58 @@
-#include <string>
-
-#ifndef M_SYSTEM_H__
-#error <m_system.h> have to be included before
+#ifndef UNICODE	//FIXME Build without UNICODE flag
+#define _AtlGetConversionACP() CP_THREAD_ACP
 #endif
 
-typedef std::string astring;
+#include "MString.h"
 
 #ifdef UNICODE
-typedef std::wstring ustring;
+typedef CMStringA astring;
+typedef CMStringW wstring;
 
-class utfstring : public std::string 
+class mbstring : public astring
 {
 	// It is prohibited to initialize by char* outside, use L"xxx"
 private:
-	utfstring( const char * pChar )		: astring( pChar ) {};
-	utfstring& operator=( const char * pChar  )	{ this->operator =( pChar ); return *this; }
+	mbstring( const char * pChar )		: astring( pChar ) {};
+	mbstring& operator=( const char * pChar  )	{ this->operator =( pChar ); return *this; }
 
 public:
-	utfstring()							: astring() {};
-	utfstring( const utfstring& uStr )	: astring( uStr ) {};
-	utfstring( const wchar_t * wChar )	{ char * astr = mir_u2a_cp( wChar, CP_UTF8 ); this->assign( astr );	mir_free( astr );	};
-	utfstring( const ustring&  tStr )	{ *this = tStr.c_str(); }
-	utfstring& operator=( const ustring&  tStr  )	{ this->operator =( tStr.c_str() ); return *this; }
-	utfstring& operator=( const astring&  aStr  )	{ wchar_t * wstr = mir_a2u( aStr.c_str() );  char * astr = mir_u2a_cp( wstr, CP_UTF8);  this->assign( astr );   mir_free( astr ); mir_free( wstr ); return *this; }
-	utfstring& operator=( const wchar_t * wChar )	{ if ( wChar != NULL ) {  char * astr = mir_u2a_cp( wChar, CP_UTF8 );   this->assign( astr );   mir_free( astr );   }	else   this->assign( "" ); return *this; }
-	
-	operator ustring()					{ wchar_t * ustr = mir_a2u_cp( this->c_str(), CP_UTF8 );   ustring res( ustr );  mir_free( ustr );  return res;   }
-	operator astring()					{ wchar_t * ustr = mir_a2u_cp( this->c_str(), CP_UTF8 );   char * astr = mir_u2a( ustr );    astring res( astr );  mir_free( astr ); mir_free( ustr );  return res;   }
+	mbstring()							: astring() {};
+	mbstring( const mbstring& uStr )	: astring( uStr ) {};
+
+		
+	mbstring( const wstring&  tStr )	{ *this = tStr.c_str(); }
+	mbstring& operator=( const wstring&  tStr  )	{ this->operator =( tStr.c_str() ); return *this; }
+
+	mbstring( const wchar_t * wChar );
+	mbstring& operator=( const astring&  aStr  );
+	mbstring& operator=( const wchar_t * wChar );
+	operator wstring();
+	operator astring();
 };
 
-class tstring : public ustring
+
+class tstring : public wstring
 {
 public:
-	   tstring()							: ustring() {};
-	   tstring(const wchar_t * pwChar)		: ustring( pwChar ) {};
-	   tstring(const char * pChar)			{	if ( pChar ){ wchar_t * wstr = mir_a2u( pChar ); this->assign( wstr );	mir_free( wstr ); } 	}
-	   tstring(const astring& aStr)			{	*this = aStr.c_str(); }
-	   tstring(const utfstring& utfStr)		{	*this = utfStr; }
-	   
-	   tstring& operator=( const char * pChar )		{ if ( pChar != NULL )	 {  wchar_t * wstr = mir_a2u( pChar );   this->assign( wstr );   mir_free( wstr );   }   else   this->assign( L"" );  return *this; }
-	   tstring& operator=( const utfstring& uStr )	{ wchar_t * wstr = mir_a2u_cp( uStr.c_str(), CP_UTF8 );   this->assign( wstr );   mir_free( wstr ); return *this; }
-	   tstring& operator=( const astring& aStr )	{ this->operator =( aStr.c_str() ); return *this; }  
+	tstring()							: wstring() {};
+	tstring(const wchar_t * pwChar)		: wstring( pwChar ) {};
+	
+	
+	tstring(const astring& aStr)		{	*this = aStr.c_str(); }
+	tstring(const mbstring& utfStr)		{	*this = utfStr; }
 
-	   operator astring()					{  char * astr = mir_u2a( this->c_str() );	   astring res( astr );	   mir_free( astr );   return res;	   }
-	   operator utfstring()					{  return  utfstring( *this ); }
+	tstring(const char * pChar);
+	tstring& operator=( const char * pChar );
+	tstring& operator=( const astring& aStr );
+	tstring& operator=( const mbstring& uStr );
+	operator astring();
+	operator mbstring()					{  return mbstring( this->c_str() ); }
 };
 
 #else
-typedef std::string tstring;
-typedef std::string utfstring;
+typedef CMStringA		astring;
+typedef CMStringA		wstring;
+typedef CMStringA		tstring;
+typedef CMStringA		mbstring;
+
 #endif
