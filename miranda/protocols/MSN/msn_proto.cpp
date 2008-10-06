2767b6bb3e9dce62dc467cf7b869f16e4532e143
Boris Krasnovskiy <borkra@users.sourceforge.net>
2008-10-06 05:26:19 +0000

    Fixed idle operation
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8402 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_proto.cpp
+++ miranda/protocols/MSN/msn_proto.cpp
@@ -109,7 +109,6 @@ CMsnProto::CMsnProto( const char* aProtoName, const TCHAR* aUserName ) :
 	HookProtoEvent( ME_DB_CONTACT_DELETED,        &CMsnProto::OnContactDeleted );
 	HookProtoEvent( ME_DB_CONTACT_SETTINGCHANGED, &CMsnProto::OnDbSettingChanged );
 	HookProtoEvent( ME_MSG_WINDOWEVENT,           &CMsnProto::OnWindowEvent );
-	HookProtoEvent( ME_IDLE_CHANGED,              &CMsnProto::OnIdleChanged );
 	HookProtoEvent( ME_CLIST_PREBUILDCONTACTMENU, &CMsnProto::OnPrebuildContactMenu );
 	HookProtoEvent( ME_CLIST_GROUPCHANGE,         &CMsnProto::OnGroupChange );
 	HookProtoEvent( ME_OPT_INITIALISE,            &CMsnProto::OnOptionsInit );
@@ -294,7 +293,9 @@ int CMsnProto::OnModulesLoaded( WPARAM wParam, LPARAM lParam )
 		HookProtoEvent( szEvent, &CMsnProto::MSN_ChatInit );
 	}
 
-	MsnInitMenus();
+	HookProtoEvent( ME_IDLE_CHANGED,              &CMsnProto::OnIdleChanged );
+
+    MsnInitMenus();
 
 	InitCustomFolders();
 
