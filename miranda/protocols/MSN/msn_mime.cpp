8315d37f059971d66e0a833f87fa42ab0ce3d024
Boris Krasnovskiy <borkra@users.sourceforge.net>
2008-10-20 02:06:43 +0000

    git-svn-id: http://miranda.googlecode.com/svn/trunk@8454 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_mime.cpp
+++ miranda/protocols/MSN/msn_mime.cpp
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "msn_global.h"
 
-#pragma hdrstop
-
 /////////////////////////////////////////////////////////////////////////////////////////
 // constructors and destructor
 
