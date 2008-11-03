e02e95754dbd921e96ab6ac56d6f89874dc48bd4
Robert Rainwater <rainwater@miranda-im.org>
2008-11-03 21:09:53 +0000

    Bump stable versions
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@8492 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/IcqOscarJ/icq_constants.h
+++ miranda/protocols/IcqOscarJ/icq_constants.h
@@ -551,7 +551,7 @@
 
 
 // Internal Constants
-#define ICQ_PLUG_VERSION            0x00030A0E
+#define ICQ_PLUG_VERSION            0x00030A0F
 #define ICQ_VERSION                 8         // Protocol version
 #define DC_TYPE                     DC_NORMAL // Used for DC settings
 #define MAX_CONTACTSSEND            15
--- miranda/protocols/MSN/version.h
+++ miranda/protocols/MSN/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,7,1,9
-#define __VERSION_STRING            "0.7.1.9"
-#define __VERSION_DWORD             0x00070109
+#define __FILEVERSION_STRING        0,7,1,10
+#define __VERSION_STRING            "0.7.1.10"
+#define __VERSION_DWORD             0x0007010A
