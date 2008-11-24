6e8c750646a6ad342a4c5073191eca8e00b21b64
Gennady Feldman <gena01@users.sourceforge.net>
2008-11-24 15:28:11 +0000

    Update version. Trying to match these up with my changes and the miranda builds.
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8585 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/Yahoo/version.h
+++ miranda/protocols/Yahoo/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,0,24
-#define __VERSION_STRING            "0.8.0.24"
-#define __VERSION_DWORD             0x00080018
+#define __FILEVERSION_STRING        0,8,0,25
+#define __VERSION_STRING            "0.8.0.25"
+#define __VERSION_DWORD             0x00080019
