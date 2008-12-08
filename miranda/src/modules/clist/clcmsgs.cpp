d589dc3b1ea39e68c4b098cc28ab345469c385ab
Boris Krasnovskiy <borkra@users.sourceforge.net>
2008-12-08 04:33:02 +0000

    Fixed crash during clist traverse
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8666 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/clist/clcmsgs.cpp
+++ miranda/src/modules/clist/clcmsgs.cpp
@@ -233,9 +233,9 @@ LRESULT fnProcessExternalMessages(HWND hwnd, struct ClcData *dat, UINT msg, WPAR
 		case CLGN_PARENT:
 			return group->groupId | HCONTACT_ISGROUP;
 		case CLGN_NEXT:
-			if (i >= group->cl.count)
+			if (++i >= group->cl.count)
 				return (LRESULT) (HANDLE) NULL;
-			return (LRESULT) cli.pfnContactToHItem(group->cl.items[i + 1]);
+			return (LRESULT) cli.pfnContactToHItem(group->cl.items[i]);
 		case CLGN_PREVIOUS:
 			if (i <= 0)
 				return (LRESULT) (HANDLE) NULL;
