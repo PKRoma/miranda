3aed97c31de23702bad7ae2d07d0bcc8f10910a8
Boris Krasnovskiy <borkra@users.sourceforge.net>
2008-12-16 07:01:17 +0000

    Do not send window close notification to sms contacts
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8714 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/AimOscar/services.cpp
+++ miranda/protocols/AimOscar/services.cpp
@@ -51,10 +51,14 @@ int CAimProto::OnWindowEvent(WPARAM wParam, LPARAM lParam)
 {
 	MessageWindowEventData* msgEvData  = (MessageWindowEventData*)lParam;
 
-	if (msgEvData->uType == MSG_WINDOW_EVT_CLOSE) 
+    if (msgEvData->uType == MSG_WINDOW_EVT_CLOSE) 
     {
-		if (state != 1 || !is_my_contact(msgEvData->hContact)) return 0;
-        
+	    if (state != 1 || !is_my_contact(msgEvData->hContact)) 
+            return 0;
+
+        if (getWord(msgEvData->hContact, "Status", ID_STATUS_OFFLINE) == ID_STATUS_ONTHEPHONE)
+            return 0;
+
         DBVARIANT dbv;
         if (!getString(msgEvData->hContact, AIM_KEY_SN, &dbv)) 
         {
