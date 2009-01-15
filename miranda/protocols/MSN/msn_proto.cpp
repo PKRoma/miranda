716fe047c13cd37951d2022cad14b5bcfd019c53
Piotr Piastucki <leech.miranda@gmail.com>
2009-01-15 19:59:27 +0000

    Fix netlib user name in AIM and MSN protocols
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8851 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_proto.cpp
+++ miranda/protocols/MSN/msn_proto.cpp
@@ -203,6 +203,13 @@ CMsnProto::~CMsnProto()
 int CMsnProto::OnModulesLoaded( WPARAM wParam, LPARAM lParam )
 {
 	char szBuffer[ MAX_PATH ], szDbsettings[64];;
+	char *name;
+
+#ifdef UNICODE
+	name = mir_u2a(m_tszUserName);
+#else
+	name = m_tszUserName;
+#endif
 
 	NETLIBUSER nlu1 = {0};
 	nlu1.cbSize = sizeof( nlu1 );
@@ -211,7 +218,7 @@ int CMsnProto::OnModulesLoaded( WPARAM wParam, LPARAM lParam )
 	nlu1.szDescriptiveName = szBuffer;
 
 	mir_snprintf( szDbsettings, sizeof(szDbsettings), "%s_HTTPS", m_szModuleName );
-	mir_snprintf( szBuffer, sizeof(szBuffer), MSN_Translate("%s plugin HTTPS connections"), m_szModuleName );
+	mir_snprintf( szBuffer, sizeof(szBuffer), MSN_Translate("%s plugin HTTPS connections"), name );
 	hNetlibUserHttps = ( HANDLE )MSN_CallService( MS_NETLIB_REGISTERUSER, 0, ( LPARAM )&nlu1 );
 
 	NETLIBUSER nlu = {0};
@@ -228,7 +235,7 @@ int CMsnProto::OnModulesLoaded( WPARAM wParam, LPARAM lParam )
 		nlu.pfnHttpGatewayUnwrapRecv = msn_httpGatewayUnwrapRecv;
 	}
 
-	mir_snprintf( szBuffer, sizeof(szBuffer), MSN_Translate("%s plugin connections"), m_szModuleName );
+	mir_snprintf( szBuffer, sizeof(szBuffer), MSN_Translate("%s plugin connections"), name );
 	hNetlibUser = ( HANDLE )MSN_CallService( MS_NETLIB_REGISTERUSER, 0, ( LPARAM )&nlu );
 
 	if ( getByte( "UseIeProxy", 0 )) {
@@ -275,6 +282,9 @@ int CMsnProto::OnModulesLoaded( WPARAM wParam, LPARAM lParam )
 			MSN_CallService(MS_NETLIB_SETUSERSETTINGS, WPARAM(hNetlibUserHttps), LPARAM(&nls));
 		}	
 	}
+#ifdef UNICODE
+	mir_free(name);
+#endif
 
 	if ( msnHaveChatDll ) 
 	{
