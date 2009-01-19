dd93515eeb4b207377760b96a73fbbb62164680a
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-01-19 03:50:31 +0000

    Copyright update
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8883 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_soapstore.cpp
+++ miranda/protocols/MSN/msn_soapstore.cpp
@@ -1,6 +1,6 @@
 /*
 Plugin of Miranda IM for communicating with users of the MSN Messenger protocol.
-Copyright (c) 2007 Boris Krasnovskiy.
+Copyright (c) 2007-2009 Boris Krasnovskiy.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -13,8 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "msn_global.h"
