8d0ff500bf2a70cff7d62edc2276048fd38d1ce6
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-01-24 18:25:59 +0000

    Reverted m_plugins.h for now Fixed caps conflict due to undocumented caps used
    git-svn-id: http://miranda.googlecode.com/svn/trunk@8906 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/include/m_plugins.h
+++ miranda/include/m_plugins.h
@@ -40,11 +40,11 @@ typedef struct PLUGIN_DB_ENUM {
 #define MS_PLUGINS_ENUMDBPLUGINS "Plugins/DbEnumerate"
 
 
-//#define DEFMOD_PROTOCOLICQ  1     //removed from v0.3 alpha
-//#define DEFMOD_PROTOCOLMSN  2	    //removed from v0.1.2.0+
+#define DEFMOD_PROTOCOLICQ	   1    //removed from v0.3 alpha
+#define DEFMOD_PROTOCOLMSN	   2	  //removed from v0.1.2.0+
 #define DEFMOD_UIFINDADD      3
 #define DEFMOD_UIUSERINFO     4
-//#define DEFMOD_SRMESSAGE    5     //removed from v0.3.3a (temp)
+#define DEFMOD_SRMESSAGE      5	  //removed from v0.3.3a (temp)
 #define DEFMOD_SRURL          6
 #define DEFMOD_SREMAIL        7
 #define DEFMOD_SRAUTH         8
@@ -52,23 +52,22 @@ typedef struct PLUGIN_DB_ENUM {
 #define DEFMOD_UIHELP         10
 #define DEFMOD_UIHISTORY      11
 //#define DEFMOD_RNDCHECKUPD  12    //removed from v0.3.1 alpha
-//#define DEFMOD_RNDICQIMPORT 13	//removed from v0.3 alpha
+//#define DEFMOD_RNDICQIMPORT 13	   //removed from v0.3 alpha
 #define DEFMOD_RNDAUTOAWAY    14
 #define DEFMOD_RNDUSERONLINE  15
-//#define DEFMOD_RNDCRYPT     16    // v0.1.0.1-v0.1.2.0
-#define DEFMOD_SRAWAY         17	// v0.1.0.1+
-#define DEFMOD_RNDIGNORE      18	// v0.1.0.1+
+#define DEFMOD_RNDCRYPT       16    // v0.1.0.1-v0.1.2.0
+#define DEFMOD_SRAWAY         17	   // v0.1.0.1+
+#define DEFMOD_RNDIGNORE      18	   // v0.1.0.1+
 #define DEFMOD_UIVISIBILITY   19    // v0.1.1.0+, options page only
 #define DEFMOD_UICLUI         20    // v0.1.1.0+
-//#define DEFMOD_UIPLUGINOPTS 21	// removed from 0.4.0.1
+//#define DEFMOD_UIPLUGINOPTS 21	   // removed from 0.4.0.1
 //#define DEFMOD_PROTOCOLNETLIB 22	// removed from 0.8.0.5
 #define DEFMOD_RNDIDLE        23    // v0.3.4a+
-#define DEFMOD_CLISTALL       24	// v0.3.4a+ (2004/09/28)
+#define DEFMOD_CLISTALL       24	   // v0.3.4a+ (2004/09/28)
 #define DEFMOD_DB             25    // v0.3.4.3+ (2004/10/11)
 #define DEFMOD_FONTSERVICE    26    // v0.7.0+ (2006/11/17)
 #define DEFMOD_UPDATENOTIFY   27
-#define DEFMOD_SSL            28    // v0.8.0+
-#define DEFMOD_HIGHEST        28
+#define DEFMOD_HIGHEST        27
 
 //plugins/getdisabledefaultarray
 //gets an array of the modules that the plugins report they want to replace
