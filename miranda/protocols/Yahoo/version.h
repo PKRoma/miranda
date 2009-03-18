394d136f214ef2258d31cb0f02cd918100037a6a
Gennady Feldman <gena01@users.sourceforge.net>
2009-03-18 19:14:32 +0000

    Updated the version number to match build 29 (not released yet)
    git-svn-id: http://miranda.googlecode.com/svn/trunk@9167 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/Yahoo/version.h
+++ miranda/protocols/Yahoo/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,0,25
-#define __VERSION_STRING            "0.8.0.25"
-#define __VERSION_DWORD             0x00080019
+#define __FILEVERSION_STRING        0,8,0,29
+#define __VERSION_STRING            "0.8.0.29"
+#define __VERSION_DWORD             0x0008001D
