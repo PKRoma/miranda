0bd2ce1d36e62943ba0da12ad661e8fc3822e796
George Hazan <ghazan@miranda-im.org>
2009-03-22 13:41:04 +0000

    compile modernopt into a build
    git-svn-id: http://miranda.googlecode.com/svn/trunk@9196 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/bin/make.bat
+++ miranda/bin/make.bat
@@ -118,6 +118,11 @@ call :Nmake modernb.mak "modernb - Win32 Release"
 popd
 if errorlevel 1 goto :Error
 
+pushd ..\..\miranda\plugins\modernopt
+call :Nmake modernopt.mak "modernopt - Win32 Release"
+popd
+if errorlevel 1 goto :Error
+
 pushd ..\..\miranda\plugins\mwclist
 call :Nmake mwclist.mak "mwclist - Win32 Release"
 popd
--- miranda/bin/makew.bat
+++ miranda/bin/makew.bat
@@ -101,6 +101,11 @@ call :Nmake modernb.mak "modernb - Win32 Release Unicode"
 popd
 if errorlevel 1 goto :Error
 
+pushd ..\..\miranda\plugins\modernopt
+call :Nmake modernopt.mak "modernopt - Win32 Release Unicode"
+popd
+if errorlevel 1 goto :Error
+
 pushd ..\..\miranda\plugins\mwclist
 call :Nmake mwclist.mak "mwclist - Win32 Release Unicode"
 popd
