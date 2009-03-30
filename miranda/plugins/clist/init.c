93ef25b9a2c0b751032a1af668d8a3394bf52359
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-03-30 12:14:41 +0000

    x64 port
    git-svn-id: http://miranda.googlecode.com/svn/trunk@9286 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist/init.c
+++ miranda/plugins/clist/init.c
@@ -142,7 +142,7 @@ int __declspec(dllexport) CListInitialise(PLUGINLINK * link)
 	mir_getMMI( &mmi );
 
 	pcli = ( CLIST_INTERFACE* )CallService(MS_CLIST_RETRIEVE_INTERFACE, 0, (LPARAM)g_hInst);
-	if ( (int)pcli == CALLSERVICE_NOTFOUND ) {
+	if ( (INT_PTR)pcli == CALLSERVICE_NOTFOUND ) {
 LBL_Error:
 		MessageBoxA( NULL, "This version of plugin requires Miranda IM 0.8.0.9 or later", "Fatal error", MB_OK );
 		return 1;
