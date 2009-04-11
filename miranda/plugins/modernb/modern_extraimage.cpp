64213cd74b5a8387bc30af913089f80e22361f0d
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-04-11 22:27:12 +0000

    Removed calls to services that do not exist
    git-svn-id: http://miranda.googlecode.com/svn/trunk@9419 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/modernb/modern_extraimage.cpp
+++ miranda/plugins/modernb/modern_extraimage.cpp
@@ -197,7 +197,7 @@ void ExtraImage_ReloadExtraIcons()
 		
 		//calc only needed protocols
 		for(i=0;i<count;i++) {
-			if ( CallProtoService(accs[i]->szModuleName,PS_GETCAPS,PFLAGNUM_2,0)==0) continue;
+            if (!accs[i]->bDynDisabled && CallProtoService(accs[i]->szModuleName,PS_GETCAPS,PFLAGNUM_2,0)==0) continue;
 			ImageList_AddIcon(hExtraImageList,LoadSkinnedProtoIcon(accs[i]->szModuleName,ID_STATUS_ONLINE));
 		}
 
