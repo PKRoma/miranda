c2ea0d128c3a914bc4e706a03ce0e2d6a7901e43
George Hazan <ghazan@miranda-im.org>
2009-04-11 22:47:03 +0000

    local SDK headers are replaced with the Miranda's shared headers
    git-svn-id: http://miranda.googlecode.com/svn/trunk@9420 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist_nicer/commonheaders.h
+++ miranda/plugins/clist_nicer/commonheaders.h
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define _WIN32_WINNT 0x0501
 #define _WIN32_IE 0x0501
 
-#include "m_stdhdr.h"
+#include <m_stdhdr.h>
 
 #include <windows.h>
 #include <commctrl.h>
