07578644a6a1943fc62da52eace6acd23449a951
Maxim Mluhov <maxim.mluhov@gmail.com>
2009-04-14 14:01:49 +0000

    Version bump
    git-svn-id: http://miranda.googlecode.com/svn/trunk@9456 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/version.h
+++ miranda/protocols/JabberG/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,0,21
-#define __VERSION_STRING            "0.8.0.21"
-#define __VERSION_DWORD             0x00080015
+#define __FILEVERSION_STRING        0,8,0,22
+#define __VERSION_STRING            "0.8.0.22"
+#define __VERSION_DWORD             0x00080016
