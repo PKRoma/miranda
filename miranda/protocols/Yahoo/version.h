a32ff10905102b1b9c73826da8bfe4a6e70b148a
Gennady Feldman <gena01@users.sourceforge.net>
2009-04-18 01:50:50 +0000

    Changing the trunk version.
    git-svn-id: http://miranda.googlecode.com/svn/trunk@9511 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/Yahoo/version.h
+++ miranda/protocols/Yahoo/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,0,30
-#define __VERSION_STRING            "0.8.0.30"
-#define __VERSION_DWORD             0x0008001E
+#define __FILEVERSION_STRING        0,9,0,1
+#define __VERSION_STRING            "0.9.0.1"
+#define __VERSION_DWORD             0x00090001
