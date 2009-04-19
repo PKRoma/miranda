65be949cc3e474eac2bcfe744cb54d308aab609e
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-04-19 18:31:10 +0000

    One more port binding fix
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@9533 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/netlib/netlibbind.cpp
+++ miranda/src/modules/netlib/netlibbind.cpp
@@ -36,7 +36,11 @@ bool BindSocketToPort(const char *szPorts, SOCKET s, int* portn)
     if (--*portn < 0 && s != INVALID_SOCKET)
     {
         BindSocketToPort(szPorts, INVALID_SOCKET, portn);
-        if (*portn == 0) return false;
+        if (*portn == 0)
+        {
+            LeaveCriticalSection(&csNetlibUser);
+            return false;
+        }
         WORD num;
         CallService(MS_UTILS_GETRANDOM, sizeof(WORD), (LPARAM)&num);
         *portn = num % *portn;
