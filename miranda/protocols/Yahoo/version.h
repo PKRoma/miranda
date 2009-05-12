884a860b0ee7d3a88aa010812a60f59f3df85cd5
Gennady Feldman <gena01@users.sourceforge.net>
2009-05-12 12:01:46 +0000

    Changing version to match next miranda build. We got out of sync in the last few builds.
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@9753 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/Yahoo/version.h
+++ miranda/protocols/Yahoo/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,0,31
-#define __VERSION_STRING            "0.8.0.31"
-#define __VERSION_DWORD             0x0008001F
+#define __FILEVERSION_STRING        0,8,0,34
+#define __VERSION_STRING            "0.8.0.34"
+#define __VERSION_DWORD             0x00080022
