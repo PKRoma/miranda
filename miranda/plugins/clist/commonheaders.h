e57864fe553953138f6c7d763a37759c2ef2e1d3
George Hazan <ghazan@miranda-im.org>
2009-05-17 17:40:06 +0000

    compilation fix for clist classic
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@9794 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/clist/commonheaders.h
+++ miranda/plugins/clist/commonheaders.h
@@ -58,7 +58,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_file.h>
 #include <m_addcontact.h>
 #include "clc.h"
-#include "clist.h"
 
 // shared vars
 extern HINSTANCE g_hInst;
