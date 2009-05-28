94dc30eedb2884c5636e58e2a3f0b4565241f03f
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-05-28 12:00:40 +0000

    Fixed ssl eating data at connection termination
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@9869 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/netlib/netlibsock.cpp
+++ miranda/src/modules/netlib/netlibsock.cpp
@@ -23,7 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "commonheaders.h"
 #include "netlib.h"
 
-extern CRITICAL_SECTION csNetlibCloseHandle;
 extern HANDLE hConnectionHeaderMutex,hSendEvent,hRecvEvent;
 
 INT_PTR NetlibSend(WPARAM wParam,LPARAM lParam)
