6e1475883cb5162905d5e3f94b40d1954afe56a0
Piotr Piastucki <leech.miranda@gmail.com>
2009-06-20 05:53:24 +0000

    bump version
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@10143 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/Scriver/srmm.c
+++ miranda/plugins/Scriver/srmm.c
@@ -38,7 +38,7 @@ PLUGININFOEX pluginInfo = {
 #else
 	"Scriver",
 #endif
-	PLUGIN_MAKE_VERSION(2, 8, 0, 42),
+	PLUGIN_MAKE_VERSION(2, 8, 1, 0),
 	"Scriver - send and receive instant messages",
 	"Miranda IM Development Team",
 	"the_leech@users.berlios.de",
