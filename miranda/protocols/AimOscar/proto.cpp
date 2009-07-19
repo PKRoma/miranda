29ee15597db4feb5f5cc4f0ab9f030d477b91052
Boris Krasnovskiy <borkra@gmail.com>
2009-07-19 00:04:59 +0000

    Removed encrypted HTML handling due to problems with MetaContacts
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@10371 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/AimOscar/proto.cpp
+++ miranda/protocols/AimOscar/proto.cpp
@@ -453,17 +453,7 @@ int __cdecl CAimProto::RecvMsg(HANDLE hContact, PROTORECVEVENT* pre)
 {
 	CCSDATA ccs = { hContact, PSR_MESSAGE, 0, (LPARAM)pre };
 
-    char *bbuf = NULL;
-    if (getByte(AIM_KEY_FI, 0)) 		
-    {
-        LOG("Converting from html to bbcodes then stripping leftover html.");
-       pre->szMessage = bbuf = html_to_bbcodes(pre->szMessage);
-    }
-    LOG("Stripping html.");
-    html_decode(pre->szMessage);
-
     int res = (int)CallService(MS_PROTO_RECVMSG, 0, (LPARAM)&ccs);
-    mir_free(bbuf);
 
     return res;
 }
