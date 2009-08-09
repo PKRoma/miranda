6b39298de38e3e7ae400b525861e2d06f3dffc55
Boris Krasnovskiy <borkra@gmail.com>
2009-08-09 05:37:22 +0000

    Fixed using deallocated memory
    git-svn-id: http://miranda.googlecode.com/svn/trunk@10522 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/Scriver/msgdialog.c
+++ miranda/plugins/Scriver/msgdialog.c
@@ -2434,8 +2434,8 @@ INT_PTR CALLBACK DlgProcMessage(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lP
 			ieWindow.hwnd = dat->windowData.hwndLog;
 			CallService(MS_IEVIEW_WINDOW, 0, (LPARAM)&ieWindow);
 		}
-		mir_free(dat);
 		NotifyLocalWinEvent(dat->windowData.hContact, hwndDlg, MSG_WINDOW_EVT_CLOSE);
+		mir_free(dat);
 		break;
 	}
 	return FALSE;
