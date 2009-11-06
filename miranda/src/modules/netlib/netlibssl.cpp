4b08f3f29b545cc9475041a1f3b091daede38781
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-11-06 04:07:04 +0000

    More Win9x SSL improvements
    git-svn-id: http://miranda.googlecode.com/svn/trunk@10970 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/netlib/netlibssl.cpp
+++ miranda/src/modules/netlib/netlibssl.cpp
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define SECURITY_WIN32
 #include <security.h>
-#include "schannel.h"
+#include <schannel.h>
 
 //#include <SCHNLSP.H>
 
