e44e37fbe1766981dce12e18f1339ba6093e89d3
Boris Krasnovskiy <borkra@users.sourceforge.net>
2009-12-05 14:07:31 +0000

    Added accessibility to chat (Fixed issue 370)
    git-svn-id: http://miranda.googlecode.com/svn/trunk@11027 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/chat/window.c
+++ miranda/plugins/chat/window.c
@@ -19,6 +19,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "chat.h"
+#include <Initguid.h >
+#include <Oleacc.h>
 
 extern HBRUSH		hEditBkgBrush;
 extern HBRUSH		hListBkgBrush;
@@ -40,6 +42,7 @@ static WNDPROC OldTabProc;
 static WNDPROC OldFilterButtonProc;
 static WNDPROC OldLogProc;
 static HKL hkl = NULL;
+static IAccPropServices* pAccPropServicesForNickList = NULL;
 
 typedef struct
 {
@@ -1181,7 +1184,8 @@ INT_PTR CALLBACK RoomWndProc(HWND hwndDlg,UINT uMsg,WPARAM wParam,LPARAM lParam)
 			SESSION_INFO* psi = (SESSION_INFO*)lParam;
 			int mask;
 			HWND hNickList = GetDlgItem(hwndDlg,IDC_LIST);
-
+			HRESULT hr = CoCreateInstance(&CLSID_AccPropServices, NULL, CLSCTX_SERVER, &IID_IAccPropServices, /*(void**)*/&pAccPropServicesForNickList);
+			if (FAILED(hr)) pAccPropServicesForNickList=NULL;
 			TranslateDialogDefault(hwndDlg);
 			SetWindowLongPtr(hwndDlg,GWLP_USERDATA,(LONG_PTR)psi);
 			OldSplitterProc=(WNDPROC)SetWindowLongPtr(GetDlgItem(hwndDlg,IDC_SPLITTERX),GWLP_WNDPROC,(LONG_PTR)SplitterSubclassProc);
@@ -1899,6 +1903,7 @@ END_REMOVETAB:
 					SetTextColor(dis->hDC, ui->iStatusEx == 0?g_Settings.crUserListColor:g_Settings.crUserListHeadingsColor);
 					TextOut(dis->hDC, dis->rcItem.left+x_offset, dis->rcItem.top, ui->pszNick, lstrlen(ui->pszNick));
 					SelectObject(dis->hDC, hOldFont);
+					if (pAccPropServicesForNickList) pAccPropServicesForNickList->lpVtbl->SetHwndPropStr(pAccPropServicesForNickList,GetDlgItem(hwndDlg,IDC_LIST), OBJID_CLIENT, dis->itemID+1, PROPID_ACC_NAME, ui->pszNick);
 				}
 				return TRUE;
 		}	}
@@ -2722,7 +2727,7 @@ LABEL_SHOWWINDOW:
 		}
 		DestroyWindow( si->hwndTooltip );
 		si->hwndTooltip = NULL;
-
+		if (pAccPropServicesForNickList) pAccPropServicesForNickList->lpVtbl->Release(pAccPropServicesForNickList);
 		SetWindowLongPtr(hwndDlg,GWLP_USERDATA,0);
 		SetWindowLongPtr(GetDlgItem(hwndDlg,IDC_SPLITTERX),GWLP_WNDPROC,(LONG_PTR)OldSplitterProc);
 		SetWindowLongPtr(GetDlgItem(hwndDlg,IDC_SPLITTERY),GWLP_WNDPROC,(LONG_PTR)OldSplitterProc);
