33e988efd8c33c0463c2f1712b66ae9e15cdde49
Gennady Feldman <gena01@gmail.com>
2010-01-15 17:08:35 +0000

    Updating version
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@11144 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/Yahoo/version.h
+++ miranda/protocols/Yahoo/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,5,0
-#define __VERSION_STRING            "0.8.5.0"
-#define __VERSION_DWORD             0x00080500
+#define __FILEVERSION_STRING        0,8,6,0
+#define __VERSION_STRING            "0.8.6.0"
+#define __VERSION_DWORD             0x00080600
