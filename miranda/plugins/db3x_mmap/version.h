110be03832df560a7caf86a01d5d7024a41bae9a
Boris Krasnovskiy <borkra@users.sourceforge.net>
2010-01-22 00:20:12 +0000

    Fixed version
    git-svn-id: http://miranda.googlecode.com/svn/trunk@11172 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/db3x_mmap/version.h
+++ miranda/plugins/db3x_mmap/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,0,0
-#define __VERSION_STRING            "0.0.0.0"
-#define __VERSION_DWORD             0x00080000
+#define __FILEVERSION_STRING        0,8,1,0
+#define __VERSION_STRING            "0.8.1.0"
+#define __VERSION_DWORD             0x00080100
