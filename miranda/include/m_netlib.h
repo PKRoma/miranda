97f9551eaff19e1e0d2d6c740d3e81ed7a95979d
Boris Krasnovskiy <borkra@users.sourceforge.net>
2010-02-17 03:11:47 +0000

    Fixed vc6 compilation
    git-svn-id: http://miranda.googlecode.com/svn/trunk@11256 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/include/m_netlib.h
+++ miranda/include/m_netlib.h
@@ -708,7 +708,10 @@ typedef struct
 //#include <stdarg.h> and <stdio.h> before including this header in order to
 //use it.
 #if defined va_start && (defined _STDIO_DEFINED || defined _STDIO_H_) && (!defined NETLIB_NOLOGGING)
-__declspec(noinline) static INT_PTR Netlib_Logf(HANDLE hUser,const char *fmt,...)
+#if _MSC_VER > 1200
+__declspec(noinline)
+#endif
+static INT_PTR Netlib_Logf(HANDLE hUser,const char *fmt,...)
 {
 	va_list va;
 	char szText[1024];
