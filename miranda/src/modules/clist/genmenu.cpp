96e56d9190b4bce5283c47d953b83d00cbbad033
Boris Krasnovskiy <borkra@users.sourceforge.net>
2010-03-20 22:05:21 +0000

    Fixed crashes with some cists
    git-svn-id: http://miranda.googlecode.com/svn/trunk@11423 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/clist/genmenu.cpp
+++ miranda/src/modules/clist/genmenu.cpp
@@ -336,7 +336,7 @@ INT_PTR MO_MenuItemGetOwnerData(WPARAM wParam, LPARAM)
 PMO_IntMenuItem MO_GetIntMenuItem(HGENMENU wParam)
 {
 	PMO_IntMenuItem result = ( PMO_IntMenuItem )wParam;
-	if ( result == NULL )
+	if ( result == NULL || wParam == (HGENMENU)0xffff1234 )
 		return NULL;
 
 	__try
