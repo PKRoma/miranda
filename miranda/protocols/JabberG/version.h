51e81c037d2f88cae3b9930ec53461c342940229
Robert Rainwater <rainwater@miranda-im.org>
2010-03-21 20:15:18 +0000

    Bump stable version
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@11431 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/version.h
+++ miranda/protocols/JabberG/version.h
@@ -1,3 +1,3 @@
-#define __FILEVERSION_STRING        0,8,9,2
-#define __VERSION_STRING            "0.8.9.2"
-#define __VERSION_DWORD             PLUGIN_MAKE_VERSION(0, 8, 9, 2)
+#define __FILEVERSION_STRING        0,8,10,0
+#define __VERSION_STRING            "0.8.10.0"
+#define __VERSION_DWORD             PLUGIN_MAKE_VERSION(0, 8, 10, 0)
