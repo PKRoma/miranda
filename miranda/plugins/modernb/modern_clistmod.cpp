3043363bbcf67ca889c964a549bf2f37be8610d2
Boris Krasnovskiy <borkra@users.sourceforge.net>
2010-03-27 00:03:43 +0000

    Changed off screen repositioning to use core service
    git-svn-id: http://miranda.googlecode.com/svn/trunk@11473 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/modernb/modern_clistmod.cpp
+++ miranda/plugins/modernb/modern_clistmod.cpp
@@ -522,11 +522,8 @@ int cliShowHide(WPARAM wParam,LPARAM lParam)
 	}
 
 	if( (bShow == TRUE || lParam == 1) ) {
-		HMONITOR (WINAPI *MyMonitorFromWindow)(HWND,DWORD);
-		RECT rcScreen,rcWindow;
-		int offScreen=0;
+		RECT rcWindow;
 
-		SystemParametersInfo(SPI_GETWORKAREA,0,&rcScreen,FALSE);
 		GetWindowRect(pcli->hwndContactList,&rcWindow);
 
 		Sync( CLUIFrames_ActivateSubContainers, TRUE );
@@ -548,31 +545,13 @@ int cliShowHide(WPARAM wParam,LPARAM lParam)
 			SetForegroundWindow(pcli->hwndContactList);
 		}
 		ModernWriteSettingByte(NULL,"CList","State",SETTING_STATE_NORMAL);
-		//this forces the window onto the visible screen
-		MyMonitorFromWindow=(HMONITOR (WINAPI *)(HWND,DWORD))GetProcAddress(GetModuleHandle(TEXT("USER32")),"MonitorFromWindow");
-		if(MyMonitorFromWindow) {
-			if(MyMonitorFromWindow(pcli->hwndContactList,0)==NULL) {
-				BOOL (WINAPI *MyGetMonitorInfoA)(HMONITOR,LPMONITORINFO);
-				MONITORINFO mi={0};
-				HMONITOR hMonitor=MyMonitorFromWindow(pcli->hwndContactList,2);
-				MyGetMonitorInfoA=(BOOL (WINAPI *)(HMONITOR,LPMONITORINFO))GetProcAddress(GetModuleHandle(TEXT("USER32")),"GetMonitorInfoA");
-				mi.cbSize=sizeof(mi);
-				MyGetMonitorInfoA(hMonitor,&mi);
-				rcScreen=mi.rcWork;
-				offScreen=1;
-			}
+
+		if (Utils_AssertInsideScreen(&rcWindow) == 1)
+		{
+			MoveWindow(cli.hwndContactList, rcWindow.left, rcWindow.top, 
+				rcWindow.right - rcWindow.left, rcWindow.bottom - rcWindow.top, TRUE);
 		}
-		else {
-			RECT rcDest;
-			if(IntersectRect(&rcDest,&rcScreen,&rcWindow)==0) offScreen=1;
-		}
-		if(offScreen) {
-			if(rcWindow.top>=rcScreen.bottom) OffsetRect(&rcWindow,0,rcScreen.bottom-rcWindow.bottom);
-			else if(rcWindow.bottom<=rcScreen.top) OffsetRect(&rcWindow,0,rcScreen.top-rcWindow.top);
-			if(rcWindow.left>=rcScreen.right) OffsetRect(&rcWindow,rcScreen.right-rcWindow.right,0);
-			else if(rcWindow.right<=rcScreen.left) OffsetRect(&rcWindow,rcScreen.left-rcWindow.left,0);
-			SetWindowPos(pcli->hwndContactList,0,rcWindow.left,rcWindow.top,rcWindow.right-rcWindow.left,rcWindow.bottom-rcWindow.top,SWP_NOZORDER);
-		}
+
 		//if (DBGetContactSettingByte(NULL,"CList","OnDesktop",SETTING_ONDESKTOP_DEFAULT))
 		//    SetWindowPos(pcli->hwndContactList, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 
