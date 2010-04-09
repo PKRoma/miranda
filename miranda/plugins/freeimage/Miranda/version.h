635661a8291e767866e6dc812599e4452cd73d50
George Hazan <george.hazan@gmail.com>
2010-04-09 18:38:22 +0000

    fix for resource compiler's errors (not to change all projects)
    git-svn-id: http://miranda.googlecode.com/svn/trunk@11560 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/freeimage/Miranda/version.h
+++ miranda/plugins/freeimage/Miranda/version.h
@@ -1,4 +1,4 @@
-#include <m_version.h>
+#include "../../../include/m_version.h"
 
 #define __FILEVERSION_STRING        MIRANDA_VERSION_FILEVERSION
 #define __VERSION_STRING            MIRANDA_VERSION_STRING
