c28258e947c0c5d908cd2dde5436d898b52cdcc3
Michael Kunz <michael.kunz@s2005.tu-chemnitz.de>
2010-05-23 14:55:37 +0000

    fix for rev 11812 and tool style contact list windows.
    git-svn-id: http://miranda.googlecode.com/svn/trunk@11819 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/clist/clui.cpp
+++ miranda/src/modules/clist/clui.cpp
@@ -321,7 +321,7 @@ int LoadCLUIModule(void)
 	Utils_AssertInsideScreen(&pos);
 
 	cli.hwndContactList = CreateWindowEx(
-		WS_EX_APPWINDOW | (DBGetContactSettingByte(NULL, "CList", "ToolWindow", SETTING_TOOLWINDOW_DEFAULT) ? WS_EX_TOOLWINDOW : 0),
+		(DBGetContactSettingByte(NULL, "CList", "ToolWindow", SETTING_TOOLWINDOW_DEFAULT) ? WS_EX_TOOLWINDOW : WS_EX_APPWINDOW),
 		_T(MIRANDACLASS),
 		titleText,
 		WS_POPUPWINDOW | WS_THICKFRAME | WS_CLIPCHILDREN |
