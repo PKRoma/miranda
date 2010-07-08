c0f8b3ef61f25e292f6cf72438569c0cd3e68689
Artem Shpynov <ashpynov@gmail.com>
2010-07-08 10:54:29 +0000

    one more bump
    git-svn-id: http://miranda.googlecode.com/svn/trunk@12153 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/modernb/init.cpp
+++ miranda/plugins/modernb/init.cpp
@@ -49,7 +49,7 @@ int EventArea_UnloadModule();
 
 PLUGININFOEX pluginInfo = {
 	sizeof(PLUGININFOEX),
-	DEBUG_AWARE_STR "Modern Contact List '08",
+	DEBUG_AWARE_STR "Modern Contact List '09",
 	0,                              //will initialized later in MirandaPluginInfoEx
 	"Display contacts, event notifications, protocol status with advantage visual modifications. Supported MW modifications, enchanced metacontact cooperation.",
 	"Artem Shpynov, Ricardo Pescuma Domenecci and Anton Senko based on clist_mw by Bethoven",
