566eec467f1359d4971dffb148abc5c6ee8ab053
Boris Krasnovskiy <borkra@users.sourceforge.net>
2010-08-26 00:26:30 +0000

    Fixed crash on nick change
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@12459 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/IRCG/commandmonitor.cpp
+++ miranda/protocols/IRCG/commandmonitor.cpp
@@ -1840,7 +1840,7 @@ static void __stdcall sttShowNickWnd( void* param )
 	}
 	SetEvent( ppro->m_evWndCreate );
 	SetWindowText( GetDlgItem( ppro->m_nickDlg->GetHwnd(), IDC_CAPTION ), TranslateT("Change nickname"));
-	SetWindowText( GetDlgItem( ppro->m_nickDlg->GetHwnd(), IDC_TEXT    ), pmsg->parameters[2].c_str());
+	SetWindowText( GetDlgItem( ppro->m_nickDlg->GetHwnd(), IDC_TEXT    ), pmsg->parameters.getCount() > 1 ? pmsg->parameters[2].c_str() : _T(""));
 	ppro->m_nickDlg->m_Enick.SetText( pmsg->parameters[1].c_str());
 	ppro->m_nickDlg->m_Enick.SendMsg( CB_SETEDITSEL, 0, MAKELPARAM(0,-1));
 	delete pmsg;
