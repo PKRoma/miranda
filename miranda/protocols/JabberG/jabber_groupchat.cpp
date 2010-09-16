f4204a5c561e2d92ca4a374fe5cb452292696d46
George Hazan <george.hazan@gmail.com>
2010-09-16 13:37:58 +0000

    fix for the minor langpack usage
    git-svn-id: http://miranda.googlecode.com/svn/trunk@12712 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/jabber_groupchat.cpp
+++ miranda/protocols/JabberG/jabber_groupchat.cpp
@@ -1154,7 +1154,9 @@ void CJabberProto::GroupchatProcessMessage( HXML node )
 						return;
 					
 					TCHAR* tmptr = _tcsstr( tmpnick, _T("has set the subject to:")); //ejabberd
-					if ( tmptr == NULL || *tmptr == 0 )
+					if ( tmptr == NULL )
+						tmptr = _tcsstr( tmpnick, TranslateT("has set the subject to:")); //ejabberd
+   				if ( tmptr == NULL || *tmptr == 0 )
 						nick = NULL;
 					else {
 						*(--tmptr) = 0;
