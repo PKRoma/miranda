51da1eaee869df3f8c671d1b12cc2ebe1e0116d2
Boris Krasnovskiy <borkra@users.sourceforge.net>
2010-09-25 20:03:48 +0000

    Renamed GMT to UTC
    git-svn-id: http://miranda.googlecode.com/svn/trunk@12801 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/utils/timezones.cpp
+++ miranda/src/modules/utils/timezones.cpp
@@ -20,7 +20,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 implements services to handle location - based timezones, instead of
-simple GMT offsets.
+simple UTC offsets.
 */
 
 #include <commonheaders.h>
