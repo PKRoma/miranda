6de9f6ced8305cc5a930ddf897a4d7b70d648a36
Boris Krasnovskiy <borkra@users.sourceforge.net>
2010-10-25 01:06:05 +0000

    Changes WLM MirVer
    git-svn-id: http://miranda.googlecode.com/svn/trunk@13036 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/MSN/msn_commands.cpp
+++ miranda/protocols/MSN/msn_commands.cpp
@@ -72,7 +72,7 @@ void CMsnProto::sttSetMirVer(HANDLE hContact, DWORD dwValue, bool always)
 		"WLM 8.5",
 		"WLM 9.0 Beta",
 		"WLM 2009",
-		"WLM 2010",
+		"WLM 2011",
 		"WLM Unknown",
 	};
 
@@ -942,7 +942,8 @@ void CMsnProto::sttProcessNotificationMessage(char* buf, unsigned len)
 
 	ezxml_t xmlmsg = ezxml_child(xmlnot, "MSG");
 	ezxml_t xmlact = ezxml_child(xmlmsg, "ACTION");
-	ezxml_t xmltxt = ezxml_get(xmlmsg, "BODY", 0, "TEXT", -1);
+	ezxml_t xmlbdy = ezxml_child(xmlmsg, "BODY");
+	ezxml_t xmltxt = ezxml_child(xmlbdy, "TEXT");
 
 	if (xmltxt != NULL)
 	{
@@ -965,6 +966,15 @@ void CMsnProto::sttProcessNotificationMessage(char* buf, unsigned len)
 		TCHAR* alrt = mir_utf8decodeT(ezxml_txt(xmltxt));
 		MSN_ShowPopup(TranslateT("MSN Alert"), alrt, MSN_ALERT_POPUP | MSN_ALLOW_MSGBOX, fullurl);
 		mir_free(alrt);
+	}
+	else if (xmlbdy)
+	{
+		const char *txt = ezxml_txt(xmlbdy);
+		if (strstr(txt, "ABCHInternal"))
+		{
+			MSN_SharingFindMembership(true);
+			MSN_ABFind("ABFindAll", NULL, true);
+		}
 	}
 	ezxml_free(xmlnot);
 }
@@ -1539,7 +1549,7 @@ LBL_InvalidCommand:
 			newThread->mInitialContact = MSN_HContactFromEmail(data.callerEmail, data.callerNick, true, true);
 			mir_snprintf(newThread->mCookie, sizeof(newThread->mCookie), "%s %d", data.authChallengeInfo, trid);
 
-			ReleaseSemaphore(newThread->hWaitEvent, 5, NULL);
+			ReleaseSemaphore(newThread->hWaitEvent, MSN_PACKETS_COMBINE, NULL);
 
 			MSN_DebugLog("Opening caller's switchboard server '%s'...", data.newServer);
 			newThread->startThread(&CMsnProto::MSNServerThread, this);
@@ -1597,7 +1607,7 @@ LBL_InvalidCommand:
 
 				case 3:
 					// P2P Bootstrap
-					p2p_processSIP(info, msgBody, NULL);
+					p2p_processSIP(info, msgBody, NULL, hContact);
 					break;
 
 				case 10:
