225e09433a3299b307def65057b988809749a837
Alex Vie <silvercircle@gmail.com>
2010-10-29 21:23:52 +0000

    disable color keying for transparent windows
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@13071 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/tabsrmm/chat/window.cpp
+++ miranda/plugins/tabsrmm/chat/window.cpp
@@ -258,7 +258,7 @@ static void Chat_UpdateWindowState(TWindowData *dat, UINT msg)
 	if (msg == WM_ACTIVATE) {
 		if (dat->pContainer->dwFlags & CNT_TRANSPARENCY && CMimAPI::m_pSetLayeredWindowAttributes != NULL) {
 			DWORD trans = LOWORD(dat->pContainer->settings->dwTransparency);
-			CMimAPI::m_pSetLayeredWindowAttributes(dat->pContainer->hwnd, CSkin::m_ContainerColorKey, (BYTE)trans, (CSkin::m_skinEnabled ? LWA_COLORKEY : 0) | (dat->pContainer->dwFlags & CNT_TRANSPARENCY ? LWA_ALPHA : 0));
+			CMimAPI::m_pSetLayeredWindowAttributes(dat->pContainer->hwnd, CSkin::m_ContainerColorKey, (BYTE)trans, (dat->pContainer->dwFlags & CNT_TRANSPARENCY ? LWA_ALPHA : 0));
 		}
 	}
 
