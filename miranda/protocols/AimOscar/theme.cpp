6154240f4e2337211881b09f5d5643acd63cee03
Boris Krasnovskiy <borkra@users.sourceforge.net>
2011-03-23 03:40:22 +0000

    Fixed translation (Fixed issue 1305)
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@13479 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/AimOscar/theme.cpp
+++ miranda/protocols/AimOscar/theme.cpp
@@ -35,35 +35,35 @@ struct _tag_iconList
 }
 static const iconList[] =
 {
-	{	"ICQ",                    "icq",         IDI_ICQ             },
-	{	"Add",                    "add",         IDI_ADD             },
-	{	"Block",                  "block",       IDI_BLOCK           },
-	{	"Profile",                "profile",     IDI_PROFILE         },
-	{	"AOL Mail",               "mail",        IDI_MAIL            },
-	{	"AIM Icon",               "aim",         IDI_AIM             },     
-	{	"Hiptop",                 "hiptop",      IDI_HIPTOP          },
-	{	"AOL Bot",                "bot",         IDI_BOT             },
-	{	"Admin",                  "admin",       IDI_ADMIN           },
-	{	"Confirmed",              "confirm",     IDI_CONFIRMED       },
-	{	"Not Confirmed",          "uconfirm",    IDI_UNCONFIRMED     },
-	{	"Blocked list",           "away",        IDI_AWAY            },
-	{	"Idle",                   "idle",        IDI_IDLE            },
-	{	"AOL",                    "aol",         IDI_AOL             },
+	{	LPGEN("ICQ"),                    "icq",         IDI_ICQ             },
+	{	LPGEN("Add"),                    "add",         IDI_ADD             },
+	{	LPGEN("Block"),                  "block",       IDI_BLOCK           },
+	{	LPGEN("Profile"),                "profile",     IDI_PROFILE         },
+	{	LPGEN("AOL Mail"),               "mail",        IDI_MAIL            },
+	{	LPGEN("AIM Icon"),               "aim",         IDI_AIM             },
+	{	LPGEN("Hiptop"),                 "hiptop",      IDI_HIPTOP          },
+	{	LPGEN("AOL Bot"),                "bot",         IDI_BOT             },
+	{	LPGEN("Admin"),                  "admin",       IDI_ADMIN           },
+	{	LPGEN("Confirmed"),              "confirm",     IDI_CONFIRMED       },
+	{	LPGEN("Not Confirmed"),          "uconfirm",    IDI_UNCONFIRMED     },
+	{	LPGEN("Blocked list"),           "away",        IDI_AWAY            },
+	{	LPGEN("Idle"),                   "idle",        IDI_IDLE            },
+	{	LPGEN("AOL"),                    "aol",         IDI_AOL             },
 
-	{	"Foreground Color",       "foreclr",     IDI_FOREGROUNDCOLOR, "Profile Editor" },
-	{	"Background Color",       "backclr",     IDI_BACKGROUNDCOLOR, "Profile Editor" },
-	{	"Bold",                   "bold",        IDI_BOLD,            "Profile Editor" },
-	{	"Not Bold",               "nbold",       IDI_NBOLD,           "Profile Editor" },
-	{	"Italic",                 "italic",      IDI_ITALIC,          "Profile Editor" },
-	{	"Not Italic",             "nitalic",     IDI_NITALIC,         "Profile Editor" },
-	{	"Underline",              "undrln",      IDI_UNDERLINE,       "Profile Editor" },
-	{	"Not Underline",          "nundrln",     IDI_NUNDERLINE,      "Profile Editor" },
-	{	"Subscript",              "sub_scrpt",   IDI_SUBSCRIPT,       "Profile Editor" },
-	{	"Not Subscript",          "nsub_scrpt",  IDI_NSUBSCRIPT,      "Profile Editor" },
-	{	"Superscript",            "sup_scrpt",   IDI_SUPERSCRIPT,     "Profile Editor" },
-	{	"Not Superscript",        "nsup_scrpt",  IDI_NSUPERSCRIPT,    "Profile Editor" },
-	{	"Normal Script",          "norm_scrpt",  IDI_NORMALSCRIPT,    "Profile Editor" },
-	{	"Not Normal Script",      "nnorm_scrpt", IDI_NNORMALSCRIPT,   "Profile Editor" },
+	{	LPGEN("Foreground Color"),       "foreclr",     IDI_FOREGROUNDCOLOR, LPGEN("Profile Editor") },
+	{	LPGEN("Background Color"),       "backclr",     IDI_BACKGROUNDCOLOR, LPGEN("Profile Editor") },
+	{	LPGEN("Bold"),                   "bold",        IDI_BOLD,            LPGEN("Profile Editor") },
+	{	LPGEN("Not Bold"),               "nbold",       IDI_NBOLD,           LPGEN("Profile Editor") },
+	{	LPGEN("Italic"),                 "italic",      IDI_ITALIC,          LPGEN("Profile Editor") },
+	{	LPGEN("Not Italic"),             "nitalic",     IDI_NITALIC,         LPGEN("Profile Editor") },
+	{	LPGEN("Underline"),              "undrln",      IDI_UNDERLINE,       LPGEN("Profile Editor") },
+	{	LPGEN("Not Underline"),          "nundrln",     IDI_NUNDERLINE,      LPGEN("Profile Editor") },
+	{	LPGEN("Subscript"),              "sub_scrpt",   IDI_SUBSCRIPT,       LPGEN("Profile Editor") },
+	{	LPGEN("Not Subscript"),          "nsub_scrpt",  IDI_NSUBSCRIPT,      LPGEN("Profile Editor") },
+	{	LPGEN("Superscript"),            "sup_scrpt",   IDI_SUPERSCRIPT,     LPGEN("Profile Editor") },
+	{	LPGEN("Not Superscript"),        "nsup_scrpt",  IDI_NSUPERSCRIPT,    LPGEN("Profile Editor") },
+	{	LPGEN("Normal Script"),          "norm_scrpt",  IDI_NORMALSCRIPT,    LPGEN("Profile Editor") },
+	{	LPGEN("Not Normal Script"),      "nnorm_scrpt", IDI_NNORMALSCRIPT,   LPGEN("Profile Editor") },
 };
 
 static HANDLE hIconLibItem[SIZEOF(iconList)];
@@ -333,8 +333,8 @@ void add_ES_icons(CAimProto* ppro)
 
 void InitExtraIcons(void)
 {
-	hExtraAT = ExtraIcon_Register("aimaccounttype", "AIM Account Type", "AIM_aol");
-	hExtraES = ExtraIcon_Register("aimextstatus", "AIM Extended Status", "AIM_hiptop");
+	hExtraAT = ExtraIcon_Register("aimaccounttype", LPGEN("AIM Account Type"), "AIM_aol");
+	hExtraES = ExtraIcon_Register("aimextstatus", LPGEN("AIM Extended Status"), "AIM_hiptop");
 
 	if (hExtraAT == NULL)
 	{
