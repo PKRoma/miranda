19e7c77e7704f59894ed09c163c7eb17e7e9397c
Boris Krasnovskiy <borkra@users.sourceforge.net>
2011-08-06 00:57:29 +0000

    Fixed error notification when Jabber is offline
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@13756 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/protocols/JabberG/jabber_proto.cpp
+++ miranda/protocols/JabberG/jabber_proto.cpp
@@ -1140,6 +1140,7 @@ void __cdecl CJabberProto::SendMessageAckThread( void* param )
 		par->msg ? ACKRESULT_FAILED : ACKRESULT_SUCCESS, 
 		( HANDLE ) 1, ( LPARAM ) par->msg );
 	Log( "Returning from thread" );
+	delete  par;
 }
 
 static char PGP_PROLOG[] = "-----BEGIN PGP MESSAGE-----\r\n\r\n";
@@ -1151,7 +1152,7 @@ int __cdecl CJabberProto::SendMsg( HANDLE hContact, int flags, const char* pszSr
 
 	DBVARIANT dbv;
 	if ( !m_bJabberOnline || JGetStringT( hContact, "jid", &dbv )) {
-		TFakeAckParams param( hContact, Translate( "Protocol is offline or no jid" ));
+		TFakeAckParams *param = new TFakeAckParams( hContact, Translate( "Protocol is offline or no jid" ));
 		JForkThread( &CJabberProto::SendMessageAckThread, &param );
 		return 1;
 	}
