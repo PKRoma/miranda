19ff75673835f96cfae7224c2e2df3da03eec0ef
Boris Krasnovskiy <borkra@users.sourceforge.net>
2011-12-17 23:32:15 +0000

    Fixed UPnP port mapping failure with some routers
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@13959 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/netlib/netlibupnp.cpp
+++ miranda/src/modules/netlib/netlibupnp.cpp
@@ -2,7 +2,7 @@
 
 Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright 2000-2010 Miranda ICQ/IM project,
+Copyright 2000-2011 Miranda ICQ/IM project,
 all portions of this codebase are copyrighted to the people
 listed in contributors.txt.
 
@@ -451,7 +451,7 @@ retrycon:
 								if (_stricmp(lrtrimp(szRes), "Chunked") == 0)
 									acksz = hdrend - szResult;
 							}
-							else if (txtParseParam(szResult, NULL, "Connection:", "\n", szRes, sizeof(szRes)))
+							if (txtParseParam(szResult, NULL, "Connection:", "\n", szRes, sizeof(szRes)))
 							{
 								needClose = (_stricmp(lrtrimp(szRes), "close") == 0);
 							}
