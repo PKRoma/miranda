b3f9441efb4e8712ab11711dce4549de6f93e74c
Boris Krasnovskiy <borkra@users.sourceforge.net>
2011-12-18 01:15:31 +0000

    Workaround for UPnP mapping cleanup if QueryStateVariable not supported
    git-svn-id: http://miranda.googlecode.com/svn/trunk@13960 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/modules/netlib/netlibupnp.cpp
+++ miranda/src/modules/netlib/netlibupnp.cpp
@@ -2,7 +2,7 @@
 
 Miranda IM: the free IM client for Microsoft* Windows*
 
-Copyright 2000-2010 Miranda ICQ/IM project,
+Copyright 2000-2011 Miranda ICQ/IM project,
 all portions of this codebase are copyrighted to the people
 listed in contributors.txt.
 
@@ -800,7 +800,7 @@ void NetlibUPnPCleanup(void*)
 	{
 		char* szData = ( char* )alloca(4096);
 		char buf[50], lip[50];
-		unsigned i, j = 0, k, num = 0;
+		unsigned i, j = 0, k, num = 100;
 
 		WORD ports[30];
 
