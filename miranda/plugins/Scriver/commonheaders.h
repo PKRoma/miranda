315bccbfbf30cc4d8cdfa5aec50511dc81932f11
Boris Krasnovskiy <borkra@users.sourceforge.net>
2012-03-13 02:25:29 +0000

    Merged in avatar handling changes and version bump
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@14190 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/Scriver/commonheaders.h
+++ miranda/plugins/Scriver/commonheaders.h
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef SRMM_COMMONHEADERS_H
 #define SRMM_COMMONHEADERS_H
 
-#define MIRANDA_VER 0x0A00
+#define MIRANDA_VER 0x0900
 
 #define COMPILE_MULTIMON_STUBS
 #define _WIN32_WINNT 0x0501
