3f564431c6745c22b2c14e5a521947c732b271f6
Boris Krasnovskiy <borkra@users.sourceforge.net>
2012-03-16 13:37:57 +0000

    Added option to disable menu icons
    git-svn-id: http://miranda.googlecode.com/svn/trunk@14194 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/src/resource.h
+++ miranda/src/resource.h
@@ -486,6 +486,7 @@
 #define IDC_RADIO2                      1741
 #define IDC_ADDCHECK                    1742
 #define IDC_GETMOREPLUGINS              1744
+#define IDC_DISABLEMENUICONS            1745
 #define IDC_EFFECT_COLOUR_TEXT1         1853
 #define IDC_EFFECT_COLOUR_SPIN1         1854
 #define IDC_EFFECT_COLOUR_TEXT2         11803
@@ -521,7 +522,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        323
 #define _APS_NEXT_COMMAND_VALUE         40018
-#define _APS_NEXT_CONTROL_VALUE         1745
+#define _APS_NEXT_CONTROL_VALUE         1746
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
