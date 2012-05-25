6f8d96b41a0ddc29a8a2373e18d26cd2bc49feb5
Boris Krasnovskiy <borkra@users.sourceforge.net>
2012-05-25 23:50:08 +0000

    Added TranslateMenu function (Patch by wishmaster)
    git-svn-id: http://miranda.googlecode.com/svn/branches/stable@14245 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/include/m_langpack.h
+++ miranda/include/m_langpack.h
@@ -93,6 +93,10 @@ __inline static INT_PTR TranslateDialogDefault(HWND hwndDlg)
 //returns 0 on success, nonzero on failure
 #define MS_LANGPACK_TRANSLATEMENU    "LangPack/TranslateMenu"
 
+__inline static INT_PTR TranslateMenu(HMENU hMenu)
+{	return CallService(MS_LANGPACK_TRANSLATEMENU, (WPARAM)hMenu, 0);
+}
+
 //returns the codepage used in the language pack 	  v0.4.3.0+
 //wParam=0
 //lParam=0
