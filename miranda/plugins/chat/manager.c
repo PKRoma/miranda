861087afc0b473ea4e21648a97d6d5e69c338c15
Robert Rainwater <rainwater@gmail.com>
2013-09-11 17:35:25 +0000

    Fix bad conditional.  Fixes issue 1732
    git-svn-id: http://miranda.googlecode.com/svn/trunk@14441 83df23c2-51fd-11de-bb24-2d5e9b4af41d

--- miranda/plugins/chat/manager.c
+++ miranda/plugins/chat/manager.c
@@ -1332,7 +1332,7 @@ USERINFO* UM_AddUser(STATUSINFO* pStatusList, USERINFO** ppUserList, const TCHAR
 {
 	USERINFO * pTemp = *ppUserList, *pLast = NULL;
 
-	if (!pStatusList || !ppUserList || !ppUserList)
+	if (!pStatusList || !ppUserList)
 		return NULL;
 
 	while(pTemp && UM_CompareItem(pTemp, pszNick, wStatus) <= 0)
